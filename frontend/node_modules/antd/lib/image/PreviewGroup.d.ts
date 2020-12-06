f7599eed11e2ccdb71c35cff26825858407f6696
cjunxiang <e0176013@u.nus.edu>
2020-12-06 22:37:54 +0800

    Delete Node modules rookie mistake

--- frontend/node_modules/antd/lib/image/PreviewGroup.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import { GroupConsumerValue } from 'rc-image/lib/PreviewGroup';
-declare const InternalPreviewGroup: React.FC<GroupConsumerValue>;
-export default InternalPreviewGroup;
