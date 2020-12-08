f91e7c075f93f98cd008fba913d9b00b3147063e
limwenfeng <wilfred.limwf@gmail.com>
2020-12-08 17:33:31 +0800

    create dropdown

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -22,6 +22,12 @@ module.exports = {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    "prettier/prettier" : ["error",{"endOfLine": "auto"}]
+
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 }
---
+++ frontend/src/components/dropdown.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+import 'antd/dist/antd.css'
+import { Menu, Dropdown, Button, message, Space } from 'antd'
+import { DownOutlined, UserOutlined } from '@ant-design/icons'
+import { title } from 'process'
+
+function handleMenuClick() {
+  message.info('Click on menu item.')
+  console.log('click')
+}
+
+function DropDownBar({
+  menutitle,
+  menuarray,
+}: {
+  menutitle: string
+  menuarray: string[]
+}) {
+  const menu = (
+
+    
+    
+    <Menu onClick={handleMenuClick}>
+
+{menuarray.map((person, index) => (
+      <Menu.Item key={index} icon={<UserOutlined />}>
+      {person}
+    </Menu.Item>
+  ))}
+
+    
+    </Menu>
+
+  ) 
+  return (
+    <Space wrap>
+      <Dropdown overlay={menu}>
+        <Button>
+          {menutitle} <DownOutlined />
+        </Button>
+      </Dropdown>
+    </Space>
+  )
+}
+
+export default DropDownBar
