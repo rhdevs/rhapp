fc4d9cbc59161d5169a6d8540f4c9a3dce8c60b3
cjunxiang <e0176013@u.nus.edu>
2020-12-08 12:48:00 +0800

    Set-up Redux Tutorials

--- frontend/src/routes/Home/index.tsx
+++ frontend/src/routes/Home/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
 import TopNavBar from '../../components/Mobile/TopNavBar'
+import { getUpdateMockString } from '../../store/home/action'
+import { RootState } from '../../store/types'
 
 const MainContainer = styled.div`
   height: 100vh;
   width: 100vw;
   background-color: #fafaf4;
 `
-export default class Home extends React.Component {
-  leftIcon = (<button>Hello</button>)
+export default function Home() {
+  const dispatch = useDispatch()
+  const { sampleStateText } = useSelector((state: RootState) => state.home)
 
-  render() {
-    return (
-      <MainContainer>
-        <TopNavBar title={'NavBarTitle'} leftIconComponent={this.leftIcon} />
-      </MainContainer>
-    )
+  useEffect(() => {
+    //do smth @ the start
+  }, [dispatch])
+
+  const leftIcon = <button>Hello</button>
+
+  const onButtonClick = () => {
+    dispatch(getUpdateMockString())
   }
+
+  return (
+    <MainContainer>
+      <TopNavBar title={'NavBarTitle'} leftIconComponent={leftIcon} />
+      <button onClick={onButtonClick}>{sampleStateText}</button>
+    </MainContainer>
+  )
 }
