323d52cd384ba63e937ff102a411a0c686c5f894
limwenfeng <wilfred.limwf@gmail.com>
2020-12-09 13:01:11 +0800

    trying to import svg

---
+++ frontend/src/components/laundryCard.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Logo from '../assets/laundryNotiBell.svg'
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Logo />
+      </div>
+    )
+  }
+}
+
+export { Header }
