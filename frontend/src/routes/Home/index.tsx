b5b0c8d7fef0e4557ad726f3d9439f6c176012d1
limwenfeng <wilfred.limwf@gmail.com>
2020-12-09 18:30:56 +0800

    Create Template for laundry card w/o btn

--- frontend/src/routes/Home/index.tsx
+++ frontend/src/routes/Home/index.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import TopNavBar from '../../components/Mobile/TopNavBar'
 import { getUpdateMockString } from '../../store/home/action'
 import { RootState } from '../../store/types'
+<<<<<<< HEAD
 import DropDownBar from '../../components/dropdown'
 import picture from '../../assets/trial.svg'
 import tree from '../../assets/tree.svg'
@@ -11,6 +12,11 @@ import { BasicCard } from '../../components/basiccard/test2'
 import { LaundryPage } from '../../components/laundrypage/test'
 import { FacilityBooking } from '../../components/LaundryCards/template'
 import Selector from '../../components/Selector'
+=======
+import DropDownBar from '../../components/DropdownBar'
+import { FacilityBooking } from '../../components/LaundryCards/template'
+
+>>>>>>> ba1af377... Create Template for laundry card w/o btn
 const MainContainer = styled.div`
   height: 100vh;
   width: 100vw;
@@ -36,8 +42,11 @@ export default function Home() {
       <Selector SelectedValue={'Choose your block'} ValueArray={['1', '2', '3', '4']} />
       <TopNavBar title={'NavBarTitle'} leftIconComponent={leftIcon} />
       <button onClick={onButtonClick}>{sampleStateText}</button>
+<<<<<<< HEAD
       <LaundryPage />
       <BasicCard />
+=======
+>>>>>>> ba1af377... Create Template for laundry card w/o btn
       <FacilityBooking title={'In Use'} subtitle={'123'} colour={'red'} />
     </MainContainer>
   )
