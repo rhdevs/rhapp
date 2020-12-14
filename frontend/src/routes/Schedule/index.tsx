f88be4c31069d22c6e346240f61ddc6bac837848
hui444 <ygreek@gmail.com>
2020-12-15 03:35:56 +0800

    Create timetable (without events)

--- frontend/src/routes/Schedule/index.tsx
+++ frontend/src/routes/Schedule/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
 import { Menu } from 'antd'
 import { PlusOutlined, SearchOutlined, ShareAltOutlined } from '@ant-design/icons'
 
@@ -7,12 +7,19 @@ import styled from 'styled-components'
 import TopNavBar from '../../components/Mobile/TopNavBar'
 import Tags from '../../components/Mobile/Tags'
 import MenuDropdown from '../../components/Mobile/MenuDropdown'
-import BottomNavBar from '../../components/Mobile/BottomNavBar'
-import { useHistory } from 'react-router-dom'
-import { PATHS } from '../Routes'
+import { Link } from 'react-router-dom'
+
+import Timetable from '../../components/timetable/Timetable'
+import { invert } from 'lodash'
+
+const TimetableMainContainer = styled.div`
+  box-sizing: border-box;
+`
 
 const TimetableContainer = styled.div`
-  height: 44vh;
+  position: relative;
+  width: 100%;
+  padding: 0 15px;
 `
 
 const SmallContainer = styled.div`
@@ -27,14 +34,212 @@ const GroupContainer = styled.div`
 
 const Background = styled.div`
   background-color: #fafaf4;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
+`
+
+const BottomNavBar = styled.div`
+  height: 64px;
+  background-color: #fafaf4;
 `
 
 const { SubMenu } = Menu
 
+type NUSModsEvent = {
+  classNo: string
+  covidZone: string
+  day: string
+  endTime: string
+  lessonType: string
+  size: number
+  startTime: string
+  venue: string
+  weeks: number[]
+}
+
+type RHEvent = {
+  eventName: string
+  location: string
+  day: string
+  endTime: string
+  startTime: string
+}
+
+type lessonTypeAbbrev = { [abbrevLessonType: string]: string }
+export const ABBREV_TO_LESSON: lessonTypeAbbrev = {
+  DLEC: 'Design Lecture',
+  LAB: 'Laboratory',
+  LEC: 'Lecture',
+  PLEC: 'Packaged Lecture',
+  PTUT: 'Packaged Tutorial',
+  REC: 'Recitation',
+  SEC: 'Sectional Teaching',
+  SEM: 'Seminar-Style Module Class',
+  TUT: 'Tutorial',
+  TUT2: 'Tutorial Type 2',
+  TUT3: 'Tutorial Type 3',
+  WS: 'Workshop',
+}
+
+// Reverse lookup map of ABBREV_TO_LESSON
+export const LESSON_TO_ABBREV: { [lessonType: string]: string } = invert(ABBREV_TO_LESSON)
+
+export const LESSON_TYPE_SEP = ':'
+export const LESSON_SEP = ','
+
+const testLink =
+  'https://nusmods.com/timetable/sem-2/share?AC5002=LEC:1&CG1112=LAB:04,TUT:01,LEC:01&CG2023=LAB:05,PTUT:03,PLEC:02'
+//moduleCode=lessonType:classNo,lessonType:classNo
+// AC5002=LEC:1
+// CG1112=LAB:04,TUT:01,LEC:01
+// CG2023=LAB:05,PTUT:03,PLEC:02
+
 export default function Schedule() {
-  const history = useHistory()
+  const [monEvents] = useState<RHEvent[]>([])
+  const [tueEvents] = useState<RHEvent[]>([])
+  const [wedEvents] = useState<RHEvent[]>([])
+  const [thuEvents] = useState<RHEvent[]>([])
+  const [friEvents] = useState<RHEvent[]>([])
+  const [satEvents] = useState<RHEvent[]>([])
+  const [sunEvents] = useState<RHEvent[]>([])
+
+  const doSomething = () => {
+    const extractedData = extractDataFromLink(testLink)
+    console.log(extractedData)
+    for (let i = 0; i < extractedData.length; i++) {
+      NUSModsToRHEvents('2020-2021', extractedData[i])
+    }
+    const eventsArray = [monEvents, tueEvents, wedEvents, thuEvents, friEvents, satEvents, sunEvents]
+    return eventsArray
+  }
+
+  /**
+   * Returns a 2D array, containing module code and lesson information of each module
+   *
+   * @param link NUSMods share link
+   */
+  const extractDataFromLink = (link: string) => {
+    const timetableInformation = link.split('?')[1]
+    const timetableData = timetableInformation.split('&')
+    const data: string[][] = []
+    for (let i = 0; i < timetableData.length; i++) {
+      const moduleCode = timetableData[i].split('=')[0]
+      data[i] = []
+      data[i].push(moduleCode)
+      timetableData[i] = timetableData[i].split('=')[1]
+      const moduleLessons = timetableData[i].split(LESSON_SEP)
+      for (let j = 0; j < moduleLessons.length; j++) {
+        data[i].push(moduleLessons[j])
+      }
+    }
+    return data
+  }
+
+  /**
+   * Fetches data from NUSMods API, reformats lesson information to RHEvents and pushes events into respective day arrays
+   *
+   * @param acadYear academicYear of the lesson information is retrieved from NUSMods API
+   * @param moduleArray array of lessons selected by user (from link provided)
+   */
+  const NUSModsToRHEvents = (acadYear: string, moduleArray: string[]) => {
+    const moduleCode = moduleArray[0]
+    axios.get(`https://api.nusmods.com/v2/${acadYear}/modules/${moduleCode}.json`).then((res) => {
+      const moduleData = res.data.semesterData[0].timetable
+      moduleArray = moduleArray.splice(1)
+      for (let i = 0; i < moduleArray.length; i++) {
+        const lessonType = moduleArray[i].split(LESSON_TYPE_SEP)[0]
+        const classNo = moduleArray[i].split(LESSON_TYPE_SEP)[1]
+        const correspondingClassInformationArray = moduleData.filter(
+          (moduleClass: { classNo: string; lessonType: string }) => {
+            return moduleClass.classNo === classNo && moduleClass.lessonType === ABBREV_TO_LESSON[lessonType]
+          },
+        )
+        for (let i = 0; i < correspondingClassInformationArray.length; i++) {
+          const newEvent: RHEvent = {
+            eventName: moduleCode + ' ' + LESSON_TO_ABBREV[correspondingClassInformationArray[i].lessonType],
+            location: correspondingClassInformationArray[i].venue,
+            day: correspondingClassInformationArray[i].day,
+            endTime: correspondingClassInformationArray[i].endTime,
+            startTime: correspondingClassInformationArray[i].startTime,
+          }
+          switch (correspondingClassInformationArray[i].day) {
+            case 'Monday':
+              monEvents.push(newEvent)
+              break
+            case 'Tuesday':
+              tueEvents.push(newEvent)
+              break
+            case 'Wednesday':
+              wedEvents.push(newEvent)
+              break
+            case 'Thursday':
+              thuEvents.push(newEvent)
+              break
+            case 'Friday':
+              friEvents.push(newEvent)
+              break
+            case 'Saturday':
+              satEvents.push(newEvent)
+              break
+            case 'Sunday':
+              sunEvents.push(newEvent)
+              break
+            default:
+              console.log('Invalid day!')
+              break
+          }
+        }
+      }
+    })
+  }
+
+  // /**
+  //  * Returns selected class information retrived from NUSMods API
+  //  *
+  //  * Selected classes are placed in arrays as there can be 2 lesson times for a single selected class
+  //  *
+  //  * @param acadYear academicYear of the lesson information is retrieved from NUSMods API
+  //  * @param moduleArray array of lessons selected by user (from link provided)
+  //  */
+  // const fetchModuleData = (acadYear: string, moduleArray: string[]) => {
+  //   const moduleCode = moduleArray[0]
+  //   const selectedModuleInformationArray: NUSModsEvent[][] = []
+  //   axios.get(`https://api.nusmods.com/v2/${acadYear}/modules/${moduleCode}.json`).then((res) => {
+  //     const moduleData = res.data.semesterData[0].timetable
+  //     moduleArray = moduleArray.splice(1)
+  //     for (let i = 0; i < moduleArray.length; i++) {
+  //       const lessonType = moduleArray[i].split(LESSON_TYPE_SEP)[0]
+  //       const classNo = moduleArray[i].split(LESSON_TYPE_SEP)[1]
+  //       const correspondingClassInformationArray = moduleData.filter(
+  //         (moduleClass: { classNo: string; lessonType: string }) => {
+  //           return moduleClass.classNo === classNo && moduleClass.lessonType === ABBREV_TO_LESSON[lessonType]
+  //         },
+  //       )
+  //       selectedModuleInformationArray.push(correspondingClassInformationArray)
+  //     }
+  //     reformatToRHEvent(moduleCode, selectedModuleInformationArray)
+  //   })
+  // }
+
+  // const reformatToRHEvent = (moduleCode: string, selectedModuleInformationArray: Array<NUSModsEvent[]>) => {
+  //   const reformattedEventsArray: RHEvent[] = []
+  //   selectedModuleInformationArray.map((s) => {
+  //     s.map((t: NUSModsEvent) => {
+  //       const newEvent: RHEvent = {
+  //         eventName: moduleCode + ' ' + LESSON_TO_ABBREV[t.lessonType],
+  //         location: t.venue,
+  //         day: t.day,
+  //         startTime: t.startTime,
+  //         endTime: t.endTime,
+  //       }
+  //       reformattedEventsArray.push(newEvent)
+  //     })
+  //   })
+  //   console.log(reformattedEventsArray)
+  //   events.push(reformattedEventsArray)
+  //   console.log(events)
+  // }
+
   const rightIcon = (
     <MenuDropdown
       menuItem={
@@ -47,36 +252,18 @@ export default function Schedule() {
               console.log('share')
             }}
           >
-            <Menu.Item
-              key="1"
-              onClick={() => {
-                history.push(PATHS.SHARE_TIMETABLE_PAGE)
-              }}
-            >
-              Share with...
+            <Menu.Item key="1">
+              <Link to={'/shareTimetable'}>Share with...</Link>
             </Menu.Item>
 
             <Menu.Item key="2">Save as png</Menu.Item>
           </SubMenu>
           <SubMenu key="sub2" icon={<PlusOutlined />} title="Add Events">
             <Menu.Item key="3">Import an ICalander File (.ics)</Menu.Item>
-            <Menu.Item
-              key="4"
-              onClick={() => {
-                history.push(PATHS.CREATE_EVENT)
-              }}
-            >
-              Add an event
-            </Menu.Item>
+            <Menu.Item key="4">Add an event</Menu.Item>
           </SubMenu>
-          <Menu.Item
-            key="5"
-            icon={<SearchOutlined />}
-            onClick={() => {
-              history.push(PATHS.EVENT_LIST_PAGE)
-            }}
-          >
-            Events
+          <Menu.Item key="5" icon={<SearchOutlined />}>
+            <Link to={'/eventList'}>Events</Link>
           </Menu.Item>
         </>
       }
@@ -85,13 +272,25 @@ export default function Schedule() {
 
   return (
     <Background>
-      <TopNavBar title={'Timetable'} rightComponent={rightIcon} />
-      <TimetableContainer>
-        <h1>TIMETABLE</h1>
-      </TimetableContainer>
+      <TopNavBar title={'Timetable'} leftIcon={true} rightComponent={rightIcon} />
+      <TimetableMainContainer>
+        <TimetableContainer>
+          <Timetable events={doSomething()} />
+        </TimetableContainer>
+      </TimetableMainContainer>
       <GroupContainer>
         <SmallContainer>
-          <h1 style={{ color: 'black', padding: '5px 15px 0px 0px', margin: '0px', fontSize: '24px' }}>Friends</h1>
+          <h1
+            style={{
+              color: 'black',
+              padding: '5px 15px 0px 0px',
+              margin: '0px',
+              fontSize: '24px',
+              fontFamily: 'Inter',
+            }}
+          >
+            Friends
+          </h1>
         </SmallContainer>
         <Tags
           options={[
@@ -112,11 +311,21 @@ export default function Schedule() {
       </GroupContainer>
       <GroupContainer>
         <SmallContainer>
-          <h1 style={{ color: 'black', padding: '5px 15px 0px 0px', margin: '0px', fontSize: '24px' }}>Groups</h1>
+          <h1
+            style={{
+              color: 'black',
+              padding: '5px 15px 0px 0px',
+              margin: '0px',
+              fontSize: '24px',
+              fontFamily: 'Inter',
+            }}
+          >
+            Groups
+          </h1>
         </SmallContainer>
         <Tags options={['Group1', 'Group2']} />
       </GroupContainer>
-      <BottomNavBar />
+      <BottomNavBar></BottomNavBar>
     </Background>
   )
 }
