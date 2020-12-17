01c72eaaaf62b999976abf183164b61b5be4b1b1
limwenfeng <wilfred.limwf@gmail.com>
2020-12-17 13:59:26 +0800

    Added Selector to replace DropDownBar

--- frontend/src/components/Selector.tsx
+++ frontend/src/components/Selector.tsx
@@ -2,38 +2,29 @@
 import React from 'react'
 import 'antd/dist/antd.css'
 import { Space } from 'antd'
-import { Select } from 'antd';
+import { Select } from 'antd'
 import styled from 'styled-components'
 
-const { Option } = Select;
+const { Option } = Select
 
 function handleChange(value: unknown) {
-  console.log(`selected ${value}`);
+  console.log(`selected ${value}`)
 }
 
-function Selector({
-  SelectedValue, 
-  ValueArray,
-} : {
-  SelectedValue : string
-  ValueArray: string[]}) {
+function Selector({ SelectedValue, ValueArray }: { SelectedValue: string; ValueArray: string[] }) {
   const menu = (
     <>
-    <Select defaultValue={SelectedValue} style={{ width: 140 }} onChange={handleChange}>
-    {ValueArray.map((person, index) => (
-      <Option key={index} value={person}>
-      {person}
-    </Option>
-  ))}
-    </Select>
+      <Select defaultValue={SelectedValue} style={{ width: 140 }} onChange={handleChange}>
+        {ValueArray.map((person, index) => (
+          <Option key={index} value={person}>
+            {person}
+          </Option>
+        ))}
+      </Select>
     </>
   )
 
-  return (
-    <Space wrap>
-      {menu}
-    </Space>
-  )
+  return <Space wrap>{menu}</Space>
 }
 
 export default Selector
