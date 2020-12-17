d909cd34f49288c00dd506822a92bc9db4b54cc1
limwenfeng <wilfred.limwf@gmail.com>
2020-12-17 14:00:27 +0800

    DropDownBar in components but not used in index.

--- frontend/src/routes/Home/index.tsx
+++ frontend/src/routes/Home/index.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 import TopNavBar from '../../components/Mobile/TopNavBar'
 import { getUpdateMockString } from '../../store/home/action'
 import { RootState } from '../../store/types'
-import DropDownBar from '../../components/DropDownbar'
 import { FacilityBooking } from '../../components/LaundryCards/template'
 import Selector from '../../components/Selector'
 
@@ -34,8 +33,6 @@ export default function Home() {
   `
   return (
     <MainContainer>
-      <Reddropdownbar MenuTitle={'Choose your level'} MenuArray={['Level 1', 'Level 2', 'Level 3']} />
-      <Reddropdownbar MenuTitle={'Choose your block'} MenuArray={['Blk 2', 'Blk 3', 'Blk 4']} />
       <Selector SelectedValue={'Choose your block'} ValueArray={['1', '2', '3', '4']} />
       <TopNavBar title={'NavBarTitle'} leftIconComponent={leftIcon} />
       <button onClick={onButtonClick}>{sampleStateText}</button>
