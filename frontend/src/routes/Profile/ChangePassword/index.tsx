62e5364968dfdfeed44563626995150debfc4e4b
Licheng-Wu <wlc.licheng@gmail.com>
2020-12-17 21:38:25 +0800

    Add change password page

---
+++ frontend/src/routes/Profile/ChangePassword/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import BottomNavBar from '../../../components/Mobile/BottomNavBar'
+import TopNavBar from '../../../components/Mobile/TopNavBar'
+import styled from 'styled-components'
+import 'antd/dist/antd.css'
+import { Input, Space, Button } from 'antd'
+
+const MainContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #fafaf4;
+  display: flex;
+  flex-direction: column;
+  justify-content: 'center';
+  align-items: 'center';
+`
+
+const LongButton = {
+  size: 'small',
+  backgroundColor: '#DE5F4C',
+  borderColor: '#DE5F4C',
+  boxShadow: '0px 2px 0px rgba(0, 0, 0, 0.043)',
+  borderRadius: '8px',
+  margin: 'auto',
+  marginTop: '2vh',
+  display: 'block',
+}
+
+const ButtonDiv = styled.div`
+  display: flex
+//   vertical-align: middle;
+  justify-content: 'center';
+  align-items: 'center';
+  //   position: relative;
+  //   left: 40%;
+  //   margin-top: 3vh;
+  //   margin-bottom: 6vh;
+`
+
+const handleClick = () => {
+  alert('Comfirmation Modal HERE')
+}
+
+export default class ChangePassword extends React.Component {
+  render() {
+    return (
+      <>
+        <TopNavBar title={'Change Password'} leftIcon={true} />
+        <MainContainer>
+          <div>
+            <Space direction="vertical">
+              <Input.Password placeholder="Old Password" style={{ width: '80vw', borderRadius: '20px', left: '10%' }} />
+              <Input.Password placeholder="New Password" style={{ width: '80vw', borderRadius: '20px', left: '10%' }} />
+              <Input.Password
+                placeholder="Confirm Password"
+                style={{ width: '80vw', borderRadius: '20px', verticalAlign: 'middle', left: '10%' }}
+              />
+            </Space>
+          </div>
+          <ButtonDiv>
+            <Button type="primary" style={LongButton} onClick={handleClick}>
+              Change
+            </Button>
+          </ButtonDiv>
+        </MainContainer>
+        <BottomNavBar />
+      </>
+    )
+  }
+}
--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -10,6 +10,7 @@ export enum PATHS {
   LOGIN_PAGE = '/login',
   SIGNUP_PAGE = '/signup',
   PROFILE_PAGE = '/profile',
+  CHANGE_PASSWORD_PAGE = '/changePassword',
   SCHEDULE_PAGE = '/schedule',
   SHARE_TIMETABLE_PAGE = '/shareTimetable',
   EVENT_LIST_PAGE = '/eventList',
@@ -19,6 +20,7 @@ const Home = React.lazy(() => import(/* webpackChunckName: "Home" */ './Home'))
 const Login = React.lazy(() => import(/* webpackChunckName: "Login" */ './Authentication/Login'))
 const Signup = React.lazy(() => import(/* webpackChunckName: "Signup" */ './Authentication/Signup'))
 const Profile = React.lazy(() => import(/* webpackChunckName: "Profile" */ './Profile/'))
+const ChangePassword = React.lazy(() => import(/* webpackChunckName: "ChangePassword" */ './Profile/ChangePassword'))
 const Schedule = React.lazy(() => import(/* webpackChunckName: "Scheduling" */ './Schedule'))
 const ShareTimetable = React.lazy(() => import(/* webpackChunckName: "ShareTimetable" */ './Schedule/ShareTimetable'))
 const EventList = React.lazy(() => import(/* webpackChunckName: "EventList" */ './Schedule/EventList'))
@@ -33,6 +35,7 @@ export default class Routes extends React.Component {
             <Route exact path={PATHS.LOGIN_PAGE} component={Login} />
             <Route exact path={PATHS.SIGNUP_PAGE} component={Signup} />
             <Route exact path={PATHS.PROFILE_PAGE} component={Profile} />
+            <Route exact path={PATHS.CHANGE_PASSWORD_PAGE} component={ChangePassword} />
             <Route exact path={PATHS.SCHEDULE_PAGE} component={Schedule} />
             <Route exact path={PATHS.SHARE_TIMETABLE_PAGE} component={ShareTimetable} />
             <Route exact path={PATHS.EVENT_LIST_PAGE} component={EventList} />
