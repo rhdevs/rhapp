5c0e2e61c0ad5eeb7ed3e6788c258a600b770ed3
cjunxiang <e0176013@u.nus.edu>
2020-12-19 17:14:34 +0800

    Added Stores and Functionality for Main Facility List Page

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -4,8 +4,9 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { home } from './home/reducer'
 import { route } from './route/reducer'
+import { facilityBooking } from './facilityBooking/reducer'
 
-export const rootReducer = combineReducers({ home, route })
+export const rootReducer = combineReducers({ home, route, facilityBooking })
 const middlewares = [thunk]
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)))
 
