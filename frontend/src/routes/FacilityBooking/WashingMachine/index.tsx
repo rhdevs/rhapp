5690d6da6434865665f47a271531eee831fdd298
limwenfeng <wilfred.limwf@gmail.com>
2020-12-23 13:27:54 +0800

    added cards into LaundryTime

--- frontend/src/routes/FacilityBooking/WashingMachine/index.tsx
+++ frontend/src/routes/FacilityBooking/WashingMachine/index.tsx
@@ -2,7 +2,16 @@ import React, { useEffect } from 'react'
 // import { useDispatch, useSelector } from 'react-redux'
 import TopNavBar from '../../../components/Mobile/TopNavBar'
 import { useDispatch } from 'react-redux'
+import { BasicCard } from '../../../components/basiccard/test2'
+import styled from 'styled-components'
+import '../../../assets/fonts.css'
 // import { RootState } from '../../store/types'
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #fafaf4; !important
+`
 
 export default function ViewBooking() {
   const dispatch = useDispatch()
@@ -14,7 +23,10 @@ export default function ViewBooking() {
 
   return (
     <>
-      <TopNavBar title={'Laundry Time'} />
+      <MainContainer>
+        <TopNavBar title={'Laundry Time'} />
+        <BasicCard />
+      </MainContainer>
     </>
   )
 }
