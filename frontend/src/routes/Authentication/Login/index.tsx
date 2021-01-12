af5be8c160776edaea91cf1e372789bb525530ab
maxxng <maxngkaishen@gmail.com>
2021-01-12 16:44:27 +0800

    move login code into routes/Authentication/Login

--- frontend/src/routes/Authentication/Login/index.tsx
+++ frontend/src/routes/Authentication/Login/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+import { Button, Input } from 'antd'
 
 import NavBar from '../../../components/NavBar'
-import LoginCard from '../../../components/LoginCard'
+import 'antd/dist/antd.css'
+import { PATHS } from '../../Routes'
 
 const LoginContainer = styled.div`
   height: 100%;
@@ -12,13 +15,58 @@ const LoginContainer = styled.div`
   padding: 15px;
 `
 
+const AccountText = styled.text`
+  margin-top: 10px;
+  font-size: 21px;
+  display: flex;
+  justify-content: center;
+`
+
 export default function Login() {
+  const history = useHistory()
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <>
       <NavBar text="Sign in" />
-      <LoginContainer>
-        <LoginCard />
-      </LoginContainer>
+      <Input
+        placeholder="Username"
+        onChange={() => {
+          setUsername(username)
+          console.log(username)
+        }}
+      ></Input>
+      <br />
+      <br />
+      <Input
+        placeholder="Password"
+        onChange={() => {
+          setPassword(password)
+          console.log(password)
+        }}
+      ></Input>
+      <br /> <br />
+      <Button
+        type="primary"
+        block
+        onClick={() => {
+          history.push(PATHS.HOME_PAGE)
+        }}
+      >
+        Sign in
+      </Button>
+      <AccountText>Do not have an account?</AccountText>
+      <Button
+        type="primary"
+        block
+        onClick={() => {
+          history.push(PATHS.SIGNUP_PAGE)
+        }}
+      >
+        Sign Up
+      </Button>
     </>
   )
 }
