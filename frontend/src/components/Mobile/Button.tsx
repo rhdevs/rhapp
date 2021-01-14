8926e771fa0ec5e8cc7c823efb04cfb605038e69
hui444 <ygreek@gmail.com>
2021-01-14 10:08:29 +0800

    Clean up code in Button

--- frontend/src/components/Mobile/Button.tsx
+++ frontend/src/components/Mobile/Button.tsx
@@ -18,14 +18,20 @@ type Props = {
 }
 
 function Button(props: Props) {
+  const buttonInitialState = (buttonIsPressed: boolean) => {
+    if (buttonIsPressed) {
+      if (props.updatedButtonColor ?? props.isFlipButton ?? true) {
+        return 'transparent'
+      } else {
+        return props.defaultButtonColor ?? '#DE5F4C'
+      }
+    } else {
+      return props.defaultButtonColor ?? '#DE5F4C'
+    }
+  }
+
   const [buttonIsPressed, setButtonIsPressed] = useState(props.buttonIsPressed ?? false)
-  const [buttonColour, setButtonColour] = useState(
-    props.buttonIsPressed
-      ? props.updatedButtonColor ?? props.isFlipButton ?? true
-        ? 'transparent'
-        : props.defaultButtonColor ?? '#DE5F4C'
-      : props.defaultButtonColor ?? '#DE5F4C',
-  )
+  const [buttonColour, setButtonColour] = useState(buttonInitialState(props.buttonIsPressed ?? false))
   const [textColour, setTextColour] = useState(props.buttonIsPressed ? '#ff7875' : props.defaultTextColor || 'white')
 
   const successfulAdd = () => {
@@ -34,6 +40,38 @@ function Button(props: Props) {
 
   const successfulRemove = () => {
     if (props.hasSuccessMessage) message.warning('Successfully removed from schedule!')
+  }
+
+  const buttonColourChooser = (buttonIsPressed: boolean) => {
+    if (buttonIsPressed) {
+      return props.defaultButtonColor ?? '#DE5F4C'
+    } else {
+      if (props.updatedButtonColor ?? props.isFlipButton ?? true) {
+        return 'transparent'
+      } else {
+        return props.defaultButtonColor ?? '#DE5F4C'
+      }
+    }
+  }
+
+  const textColourChooser = (buttonIsPressed: boolean) => {
+    if (buttonIsPressed) {
+      return props.defaultTextColor ?? 'white'
+    } else {
+      if (props.updatedTextColor ?? props.isFlipButton ?? true) {
+        return '#ff7875'
+      } else {
+        return props.defaultTextColor ?? 'white'
+      }
+    }
+  }
+
+  const buttonDescriptionChooser = (buttonIsPressed: boolean) => {
+    if (buttonIsPressed) {
+      return props.updatedButtonDescription ?? props.defaultButtonDescription
+    } else {
+      return props.defaultButtonDescription
+    }
   }
 
   return (
@@ -51,27 +89,13 @@ function Button(props: Props) {
           props.stopPropagation && e.stopPropagation()
         }
         buttonIsPressed ? successfulRemove() : successfulAdd()
-        setButtonColour(
-          buttonIsPressed
-            ? props.defaultButtonColor ?? '#DE5F4C'
-            : props.updatedButtonColor ?? props.isFlipButton ?? true
-            ? 'transparent'
-            : props.defaultButtonColor ?? '#DE5F4C',
-        )
-        setTextColour(
-          buttonIsPressed
-            ? props.defaultTextColor ?? 'white'
-            : props.updatedTextColor ?? props.isFlipButton ?? true
-            ? '#ff7875'
-            : props.defaultTextColor ?? 'white',
-        )
+        setButtonColour(buttonColourChooser(buttonIsPressed))
+        setTextColour(textColourChooser(buttonIsPressed))
         setButtonIsPressed(!buttonIsPressed)
         if (props.onButtonClick) props.onButtonClick(buttonIsPressed)
       }}
     >
-      {buttonIsPressed
-        ? props.updatedButtonDescription ?? props.defaultButtonDescription
-        : props.defaultButtonDescription}
+      {buttonDescriptionChooser(buttonIsPressed)}
     </AntdButton>
   )
 }
