de87d87b1f9ff881309a42dc1980fe65fe1f7ac4
brandoncjh <brandonchongjh@gmail.com>
2021-01-14 18:33:42 +0800

    Update auth API: change jwt generation timedelta to 1 min, draft out protected route and corresponding wrapper function

--- backend/authentication/auth.py
+++ backend/authentication/auth.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify, request, make_response
 import jwt
 import datetime
+import pymongo
+from functools import wraps
 
 #MongoDB
 myclient = client = pymongo.MongoClient("mongodb+srv://rhdevs-db-admin:rhdevs-admin@cluster0.0urzo.mongodb.net/RHApp?retryWrites=true&w=majority")
 db = myclient["RHApp"]
 
 app = flask.Flask("rhapp")
-app.config['AUTH_SECRET_KEY'] = 'secretkeyvalue'
+app.config['SECRET_KEY'] = 'secretkeyvalue' # will replace with flaskenv variable in the future
 
 CORS(app, origins=CROSS_ORIGINS_LIST, headers=['Content-Type'], 
          expose_headers=['Access-Control-Allow-Origin'], supports_credentials=True)
@@ -22,8 +24,32 @@ def userIDAlreadyExists(testID):
 
 
 """
+Decorative function: 
+checks for and verifies token. Used in /protected
+"""
+def check_for_token(func):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        #extract token; for example here I assume it is passed as query parameter
+        token = requests.args.get('token')
+
+        if not token:
+            return jsonify({'message': 'Token is missing'}), 403
+        
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            currentUser = db.User.find({'userID': { "$in": username}}).limit(1)
+        except: 
+            return jsonify({'message': 'Token is invalid'}), 403
+        
+        return f(currentUser, *args, **kwargs)
+
+    return decorated
+
+
+"""
 Register route:
-Within POST request, obtain userID, password and email and add to User table in Mongo
+Within POST request, obtain userID, password and email and add to User table in Mongo, if userID has not been registered previously
 If successful return 200, else return 500
 """
 @app.route('/auth/register', methods=['POST'])
@@ -55,21 +81,23 @@ def login():
     username = req['userID']
     passwordHash = req['passwordHash']
     #authenticate the credentials
-    if db.User.find({'userID': { "$in": username}, 'passwordHash': { "$in": passwordHash}}).count() == 0:
+    if not db.User.find({'userID': { "$in": username}, 'passwordHash': { "$in": passwordHash}}).limit(1):
         return jsonify({'message': 'Invalid credentials'}), 403
     #generate JWT
-    token = jwt.encode({'user': req.username,
-                        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15)}, app.config['AUTH_SECRET_KEY'])
+    token = jwt.encode({'user': username,
+                        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60) #to change timedelta to 15 minutes in production
+                        }, app.config['SECRET_KEY']) 
     
     return jsonify({'token': token.decode('UTF-8')}), 200
 
 
 
-# this route will only be seen if you are authenticated ie valid JWT 
+# this route acts as gatekeeper: can only access requested resource if you are authenticated ie valid JWT 
 @app.route('/auth/protected', methods=['POST'])
-def protected():
-    return {message: f'protected endpoint (allowed user {flask_praetorian.current_user().username})'}
-
+@check_for_token
+def protected(currentUser):
+    return jsonify({'message': 'You need to be logged in to view this'}), 403
+    
 
 
 # this will blacklist the JWT until its expiration
