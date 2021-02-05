cd7a35d1b3f4bfd0a16fe306870da7bd69039649
Licheng-Wu <wlc.licheng@gmail.com>
2021-02-05 22:32:18 +0800

    Update UI

--- frontend/src/routes/Authentication/Signup/index.tsx
+++ frontend/src/routes/Authentication/Signup/index.tsx
@@ -6,25 +6,28 @@ import { Button, Input, Form, Select } from 'antd'
 import { PATHS } from '../../Routes'
 import 'antd/dist/antd.css'
 
+const MainContainer = styled.div`
+  height: 110vh;
+  width: 100vw;
+  background-color: #fafaf4;
+`
+
 const AccountText = styled.text`
-  margin-top: 10px;
+  padding-top: 10px;
   font-size: 21px;
   display: flex;
   justify-content: center;
 `
 
 const SignUpContainer = styled.div`
-  height: 100%;
-  width: 400px;
-  background-color: white;
-  margin: 5vh auto;
-  padding: 15px;
+  width: 70vw;
+  margin: auto;
+  background-color: #fafaf4;
   text-align: center;
   border-radius: 13px 13px 13px 13px;
 `
 
 const PostButton = styled.div`
-  margin-top: 0px;
   text-align: center;
 
   .ant-btn-primary {
@@ -37,10 +40,10 @@ const PostButton = styled.div`
     line-height: 23px;
     letter-spacing: 0em;
     text-align: center;
-    width: 72;
-    height: 33px;
+    width: 70vw;
+    height: 6vh;
     border-radius: 10px;
-    margin-top: 10px;
+    padding-top: 10px;
   }
   .ant-btn-primary:hover,
   .ant-btn-primary:focus {
@@ -56,7 +59,7 @@ const PostButton = styled.div`
     width: 72;
     height: 33px;
     border-radius: 10px;
-    margin-top: 10px;
+    padding-top: 10px;
   }
 `
 
@@ -109,70 +112,99 @@ export default function Signup() {
 
   return (
     <>
-      <SignUpContainer>
-        <p>NUS Email:</p>
-        <Input placeholder="Email" name="email" value={formData.email} onChange={(e) => onChange(e)}></Input>
-        <br /> <br />
-        <Input placeholder="Username" name="userId" value={formData.userId} onChange={(e) => onChange(e)}></Input>{' '}
-        <br />
-        <br />
-        <Input placeholder="Password" name="password" value={formData.password} onChange={(e) => onChange(e)}></Input>
-        <br /> <br />
-        <Input
-          placeholder="Confirm Password"
-          name="password2"
-          value={formData.password2}
-          onChange={(e) => onChange(e)}
-        ></Input>
-        <br /> <br />
-        <Input placeholder="Display Name" name="display" value={formData.display} onChange={(e) => onChange(e)}></Input>
-        <br /> <br />
-        <Input
-          placeholder="Telegram Handle"
-          name="telegram"
-          value={formData.telegram}
-          onChange={(e) => onChange(e)}
-        ></Input>
-        <br /> <br />
-        <Form.Item>
-          <Select placeholder="Select your block" onChange={optionChange}>
-            <Select.Option value="1">Block 1</Select.Option>
-            <Select.Option value="2">Block 2</Select.Option>
-            <Select.Option value="3">Block 3</Select.Option>
-            <Select.Option value="4">Block 4</Select.Option>
-            <Select.Option value="5">Block 5</Select.Option>
-            <Select.Option value="6">Block 6</Select.Option>
-            <Select.Option value="7">Block 7</Select.Option>
-            <Select.Option value="8">Block 8</Select.Option>
-          </Select>
-        </Form.Item>
-        <Input.TextArea
-          placeholder="Bio"
-          name="bio"
-          value={formData.bio}
-          onChange={(e) => onChange(e)}
-        ></Input.TextArea>
-        <br />
-        <br />
-        <PostButton>
-          <Button type="primary" block onClick={onSubmit}>
-            Sign Up
-          </Button>
-        </PostButton>
-        <AccountText>Have an account?</AccountText>
-        <PostButton>
-          <Button
-            type="primary"
-            block
-            onClick={() => {
-              signupHandler()
-              history.push(PATHS.LOGIN_PAGE)
-            }}
-          >
-            Login
-          </Button>
-        </PostButton>
-      </SignUpContainer>
+      <MainContainer>
+        <p style={{ marginLeft: '10vw', paddingTop: '5vw', fontSize: '24px', fontWeight: 'bold' }}>Sign Up</p>
+        <SignUpContainer>
+          <Input
+            placeholder="NUS Email"
+            name="email"
+            style={{ borderRadius: '10px' }}
+            value={formData.email}
+            onChange={(e) => onChange(e)}
+          ></Input>
+          <br /> <br />
+          <Input
+            placeholder="Username"
+            name="userId"
+            style={{ borderRadius: '10px' }}
+            value={formData.userId}
+            onChange={(e) => onChange(e)}
+          ></Input>{' '}
+          <br />
+          <br />
+          <Input.Password
+            placeholder="Password"
+            name="password"
+            style={{ borderRadius: '10px' }}
+            value={formData.password}
+            onChange={(e) => onChange(e)}
+          ></Input.Password>
+          <br /> <br />
+          <Input.Password
+            placeholder="Confirm Password"
+            name="password2"
+            style={{ borderRadius: '10px' }}
+            value={formData.password2}
+            onChange={(e) => onChange(e)}
+          ></Input.Password>
+          <br /> <br />
+          <Input
+            placeholder="Display Name"
+            name="display"
+            style={{ borderRadius: '10px' }}
+            value={formData.display}
+            onChange={(e) => onChange(e)}
+          ></Input>
+          <br /> <br />
+          <Input
+            placeholder="Telegram Handle"
+            name="telegram"
+            style={{ borderRadius: '10px' }}
+            value={formData.telegram}
+            onChange={(e) => onChange(e)}
+          ></Input>
+          <br /> <br />
+          <Form.Item>
+            <Select placeholder="Select your block" style={{ borderRadius: '10px' }} onChange={optionChange}>
+              <Select.Option value="1">Block 1</Select.Option>
+              <Select.Option value="2">Block 2</Select.Option>
+              <Select.Option value="3">Block 3</Select.Option>
+              <Select.Option value="4">Block 4</Select.Option>
+              <Select.Option value="5">Block 5</Select.Option>
+              <Select.Option value="6">Block 6</Select.Option>
+              <Select.Option value="7">Block 7</Select.Option>
+              <Select.Option value="8">Block 8</Select.Option>
+            </Select>
+          </Form.Item>
+          <Input.TextArea
+            placeholder="Bio"
+            name="bio"
+            style={{ borderRadius: '10px' }}
+            value={formData.bio}
+            onChange={(e) => onChange(e)}
+          ></Input.TextArea>
+          <br />
+          <br />
+          <PostButton>
+            <Button type="primary" block onClick={onSubmit}>
+              Sign Up
+            </Button>
+          </PostButton>
+          <AccountText>Have an account?</AccountText>
+          <PostButton>
+            <Button
+              type="primary"
+              block
+              onClick={() => {
+                signupHandler()
+                history.push(PATHS.LOGIN_PAGE)
+              }}
+            >
+              Login
+            </Button>
+          </PostButton>
+        </SignUpContainer>
+      </MainContainer>
     </>
   )
 }
