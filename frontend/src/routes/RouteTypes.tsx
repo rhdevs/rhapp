2e24f7ecc8e10fe46e0949d4b1b2a730b7c9fe0f
cjunxiang <e0176013@u.nus.edu>
2021-02-18 02:13:44 +0800

    Confirm Auth

--- frontend/src/routes/RouteTypes.tsx
+++ frontend/src/routes/RouteTypes.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Route } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 import { DOMAIN_URL, ENDPOINTS } from '../store/endpoints'
 // import { checkIsLoggedIn } from '../store/profile/action'
 import { PROFILE_ACTIONS } from '../store/profile/types'
 import { RootState } from '../store/types'
-// import { PATHS } from './Routes'
+import { PATHS } from './Routes'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const PrivateRoute = (routeProps: any) => {
@@ -44,10 +44,10 @@ export const PrivateRoute = (routeProps: any) => {
     <Route
       {...rest}
       sensitive
-      // render={(props) =>
-      //   isLoggedIn && localStorage.token ? <Component {...props} /> : <Redirect push to={PATHS.LOGIN_PAGE} />
-      // }
-      render={(props) => <Component {...props} />}
+      render={(props) =>
+        isLoggedIn && localStorage.token ? <Component {...props} /> : <Redirect push to={PATHS.LOGIN_PAGE} />
+      }
+      // render={(props) => <Component {...props} />}
     />
   )
 }
@@ -91,17 +91,9 @@ export const AuthenticateRoute = (routeProps: any) => {
 
   console.log(isLoggedIn)
   return (
-    // <Route
-    //   {...rest}
-    //   render={(props) => (!isLoggedIn && !localStorage.token ? <Component {...props} /> : <Redirect push to="/" />)}
-    // />
     <Route
       {...rest}
-      sensitive
-      // render={(props) =>
-      //   isLoggedIn && localStorage.token ? <Component {...props} /> : <Redirect push to={PATHS.LOGIN_PAGE} />
-      // }
-      render={(props) => <Component {...props} />}
+      render={(props) => (!isLoggedIn && !localStorage.token ? <Component {...props} /> : <Redirect push to="/" />)}
     />
   )
 }
