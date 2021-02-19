b0538eb430d7e45df22ef06942f7ac645b906b1f
hui444 <ygreek@gmail.com>
2021-02-19 15:56:07 +0800

    Add remove alert after 10 seconds to nusmods import

--- frontend/src/store/scheduling/action.ts
+++ frontend/src/store/scheduling/action.ts
@@ -134,7 +134,6 @@ export const fetchCurrentUserEvents = (userId: string | null, isUserEventsOnly: 
 
       let userNusModsEvents: TimetableEvent[] = await dispatch(getUserNusModsEvents(userId, false))
       if (userNusModsEvents === null) userNusModsEvents = []
-      console.log(userNusModsEvents)
       const friendsNusModsEvents: TimetableEvent[] = selectedProfileNusModsEvents
       const allNusModsEvents: TimetableEvent[] = isUserEventsOnly
         ? userNusModsEvents
