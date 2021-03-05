15c05a21d15ce4c745a814a5c31497b4da7ce496
hui444 <ygreek@gmail.com>
2021-03-06 02:08:16 +0800

    Fix global styles, import 'Inter' font

--- frontend/src/GlobalStyle.tsx
+++ frontend/src/GlobalStyle.tsx
@@ -3,9 +3,9 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   body {
     color: #fafaf4
+    font-family: Inter;
+    background-color: #fafaf4;
   }
-  font-family: Inter;
-  background-color: #fafaf4;
 `
 
 export default GlobalStyle
