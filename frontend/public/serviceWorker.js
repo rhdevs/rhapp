7aa15c86b0a6c938c415b4008f53305b07507ab7
cjunxiang <e0176013@u.nus.edu>
2021-03-09 15:11:29 +0800

    Refine images and tweaks for score

--- frontend/public/serviceWorker.js
+++ frontend/public/serviceWorker.js
@@ -1,4 +1,12 @@
 if ('serviceWorker' in navigator) {
-  // register service worker
-  navigator.serviceWorker.register('service-worker.js')
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('service-worker.js').then(
+      function (registration) {
+        console.log('Service worker registration successful with scope: ', registration.scope)
+      },
+      function (err) {
+        console.log('Service worker registration failed: ', err)
+      },
+    )
+  })
 }
