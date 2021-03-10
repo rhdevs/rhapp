f221873f60c4d321a8cc0a835a55f68c885b3998
Richard Li Defa <richardlidefa@gmail.com>
2021-03-11 01:27:08 +0800

    user_CCA/add now supports empty arrays

--- backend/social/main.py
+++ backend/social/main.py
@@ -18,7 +18,6 @@ app.config['SECRET_KEY'] = os.getenv('AUTH_SECRET_KEY')
 
 DB_USERNAME = os.getenv('DB_USERNAME')
 DB_PWD = os.getenv('DB_PWD')
-DEFAULT_PROFILE_PIC = os.getenv('DEFAULT_PROFILE_PIC')
 URL = "mongodb+srv://rhdevs-db-admin:{}@cluster0.0urzo.mongodb.net/RHApp?retryWrites=true&w=majority".format(
     DB_PWD)
 
