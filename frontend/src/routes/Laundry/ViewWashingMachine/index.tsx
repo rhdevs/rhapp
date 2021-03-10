319b5b7fceadce7e954d680501ee3cab817e5e99
simonjulianl <simonjulian21@gmail.com>
2021-03-11 00:52:57 +0800

    add reloading

--- frontend/src/routes/Laundry/ViewWashingMachine/index.tsx
+++ frontend/src/routes/Laundry/ViewWashingMachine/index.tsx
@@ -242,6 +242,7 @@ export default function ViewWashingMachine() {
                 dispatch(SetEditMode())
                 dispatch(UpdateJobDuration(machine.machineID))
                 dispatch(SetBlockLevelSelections(selectedBlock as string, selectedLevel as string))
+                location.reload()
               } else {
                 dispatch(updateMachine(WMStatus.INUSE, machine?.machineID))
                 dispatch(SetBlockLevelSelections(selectedBlock as string, selectedLevel as string))
@@ -290,9 +291,11 @@ export default function ViewWashingMachine() {
                 dispatch(SetEditMode())
                 dispatch(UpdateJobDuration(machine.machineID))
                 dispatch(SetBlockLevelSelections(selectedBlock as string, selectedLevel as string))
+                location.reload()
               } else {
                 dispatch(updateMachine(WMStatus.AVAIL, machine?.machineID))
                 dispatch(SetBlockLevelSelections(selectedBlock as string, selectedLevel as string))
+                history.back()
               }
             }}
           />
