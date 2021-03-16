4c28b6137f62e0a8a202d2853334b809bf6f7f7d
Richard Li Defa <richardlidefa@gmail.com>
2021-03-16 09:54:13 +0800

    default value of 0 for no ccaId in booking, conflict checking for PUT route

--- backend/facilityBooking/facilities.py
+++ backend/facilityBooking/facilities.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, make_response
 from flask_cors import CORS, cross_origin
-
 import pymongo
 import json
 from datetime import datetime
+from threading import Thread
 
 
 def removeObjectID(xs):
@@ -25,12 +25,13 @@ myclient = client = pymongo.MongoClient(
     "mongodb+srv://rhdevs-db-admin:rhdevs-admin@cluster0.0urzo.mongodb.net/RHApp?retryWrites=true&w=majority")
 db = myclient["RHApp"]
 
-CROSS_ORIGINS_LIST = "https://rhapp.cjunxiang.vercel.app"
-
 # Flask
 app = Flask("rhapp")
-CORS(app, origins=CROSS_ORIGINS_LIST, headers=['Content-Type'],
-     expose_headers=['Access-Control-Allow-Origin'], supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "*"}})
+# resources allowed to be accessed explicitly
+# response.headers.add("Access-Control-Allow-Origin", "*"), add this to all responses
+# if the cors still now working
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Session
 session = {}
@@ -47,45 +48,30 @@ def root_route():
     return 'What up losers'
 
 
-@app.route('/facilities/all/')
-@cross_origin()
+@app.route('/facilities/all')
+@cross_origin(supports_credentials=True)
 def all_facilities():
     try:
         data = removeObjectID(list(db.Facilities.find()))
-
     except Exception as e:
         print(e)
         return {"err": str(e)}, 400
-    response = make_response(json.dumps(
-        list(data), default=lambda o: str(o)), 200)
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
+    return make_response(json.dumps(list(data), default=lambda o: str(o)), 200)
 
 
 @app.route('/facility/<facilityID>')
-@cross_origin()
+@cross_origin(supports_credentials=True)
 def get_facility_name(facilityID):
     try:
         data = removeObjectID(
-            list(db.Bookings.find({"facilityID": int(facilityID)})))
+            list(db.Facilities.find({"facilityID": int(facilityID)})))
     except Exception as e:
         return {"err": str(e)}, 400
     return make_response(json.dumps(list(data), default=lambda o: str(o)), 200)
 
 
-@app.route('/bookings/user/<userID>')
-def user_bookings(userID):
-    try:
-        data = removeObjectID(list(db.Bookings.find({"userID": userID})))
-    except Exception as e:
-        return {"err": str(e)}, 400
-    return make_response(json.dumps(list(data), default=lambda o: str(o)), 200)
-
-
-@app.route('/bookings')
-@cross_origin()
+@app.route('/bookings', methods=["GET"])
+@cross_origin(supports_credentials=True)
 def get_all_bookings():
     try:
         data = removeObjectID(list(db.Bookings.find({})))
@@ -94,22 +80,33 @@ def get_all_bookings():
     return make_response(json.dumps(list(data), default=lambda o: str(o)), 200)
 
 
-@app.route('/booking/<bookingID>')
-@cross_origin()
+@app.route('/booking/<bookingID>', methods=["GET"])
+@cross_origin(supports_credentials=True)
 def get_one_booking(bookingID):
     try:
         data = removeObjectID(
             list(db.Bookings.find({"bookingID": int(bookingID)})))
     except Exception as e:
         return {"err": str(e)}, 400
+    return make_response(json.dumps(data, default=lambda o: str(o)), 200)
+
+
+@app.route('/bookings/user/<userID>')
+@cross_origin(supports_credentials=True)
+def user_bookings(userID):
+    try:
+        data = removeObjectID(list(db.Bookings.find({"userID": userID})))
+    except Exception as e:
+        return {"err": str(e)}, 400
     return make_response(json.dumps(list(data), default=lambda o: str(o)), 200)
 
 
 @app.route('/bookings/facility/<facilityID>/')
+@cross_origin(supports_credentials=True)
 def check_bookings(facilityID):
     try:
-        data = removeObjectID(list(db.Bookings.find({"facilityID": int(facilityID),
-                                                     "startTime": {"$gte": int(request.args.get('startDate'))}, "endTime": {"$lte": int(request.args.get('endDate'))}})))
+        data = removeObjectID(list(db.Bookings.find({"facilityID": int(facilityID), "startTime": {"$gte": int(
+            request.args.get('startDate'))}, "endTime": {"$lte": int(request.args.get('endDate'))}})))
     except Exception as e:
         return {"err": str(e)}, 400
 
@@ -134,6 +131,7 @@ def user_telegram(userID):
 
 
 @app.route('/bookings', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def add_booking():
     try:
         # if request.cookies.get("userID") == list(db.Bookings.find({"bookingID" : bookingID}))[0]['userID'] :
@@ -142,7 +140,10 @@ def add_booking():
         formData["endTime"] = int(formData["endTime"])
 
         formData["facilityID"] = int(formData["facilityID"])
-        formData["ccaID"] = int(formData["ccaID"])
+        if not formData.get("ccaID"):
+            formData["ccaID"] = int(0)
+        else:
+            formData["ccaID"] = int(formData["ccaID"])
 
         if (formData["endTime"] < formData["startTime"]):
             raise Exception("End time eariler than start time")
@@ -177,22 +178,50 @@ def add_booking():
 
 
 @app.route('/bookings/<bookingID>', methods=['GET'])
+@cross_origin(supports_credentials=True)
 def get_booking(bookingID):
     try:
-        data = removeObjectID(
+        data = listToIndexedDict(
             list(db.Bookings.find({"bookingID": int(bookingID)})))
     except Exception as e:
         print(e)
         return {"err": str(e)}, 400
-    return make_response(json.dumps(list(data), default=lambda o: str(o)), 200)
+    return data, 200
 
 
 @app.route('/bookings/<bookingID>', methods=['PUT'])
+@cross_origin(supports_credentials=True)
 def edit_booking(bookingID):
     try:
-        # if request.cookies.get("userID") == list(db.Bookings.find({"bookingID" : bookingID}))[0]['userID'] :
+        formData = request.get_json()
+        formData["startTime"] = int(formData["startTime"])
+        formData["endTime"] = int(formData["endTime"])
+
+        formData["facilityID"] = int(formData["facilityID"])
+        if not formData.get("ccaID"):
+            formData["ccaID"] = int(0)
+        else:
+            formData["ccaID"] = int(formData["ccaID"])
+
+        if (formData["endTime"] < formData["startTime"]):
+            raise Exception("End time eariler than start time")
+        # else:
+        #     return {"err": "Unauthorised Access"}, 401
+
+        # Check for exisiting booking
+        # x1 <= y2 && y1 <= x2 because ranges are well-formed
+        conflict = removeObjectID(list(db.Bookings.find({"facilityID": formData.get("facilityID"),
+                                                         "endTime": {
+            "$gte": formData.get('startTime')},
+            "startTime": {
+            "$lte": formData.get('endTime')}
+        })))
+
+        if (len(conflict) != 0):
+            raise Exception("Conflict Booking")
+
         db.Bookings.update_one({"bookingID": int(bookingID)}, {
-                               "$set": request.get_json()})
+            "$set": request.get_json()})
 
         # else:
         #     return {"err": "Unauthorised Access"}, 401
@@ -204,6 +233,7 @@ def edit_booking(bookingID):
 
 
 @app.route('/bookings/<bookingID>', methods=['DELETE'])
+@cross_origin(supports_credentials=True)
 def delete_booking(bookingID):
     try:
         # if request.cookies.get("userID") == list(db.Bookings.find({"bookingID" : bookingID}))[0].get('userID') :
@@ -217,6 +247,17 @@ def delete_booking(bookingID):
     return {"message": "Booking Deleted"}, 200
 
 
-if __name__ == '__main__':
-    # app.run(threaded=True, debug=True)
-    app.run('0.0.0.0', port=8080)
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+keep_alive()
+
+# if __name__ == '__main__':
+#   keep_alive();
+#   app.run('0.0.0.0', port=8080)
