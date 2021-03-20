d8f06cc67297a8b6a8924b350af6eece34d6d4f7
hui444 <ygreek@gmail.com>
2021-03-20 15:03:34 +0800

    Add supper feature types

--- frontend/src/store/supper/types.ts
+++ frontend/src/store/supper/types.ts
@@ -1,3 +1,48 @@
+export type User = {
+  userId: string
+  profilePictureUrl: string
+  displayName: string
+  telegramHandle: string
+  block: number
+  bio: string
+  modules: string[]
+}
+
+export type Food = {
+  foodName: string
+  price: number
+  foodId: string
+  comments?: string //hopper comments
+  quantity?: number //hopper order quantity
+}
+
+export type Restaurant = {
+  menu: Food[]
+  closingTime: number
+  contactNumber: number
+}
+
+export type SubOrder = {
+  foodList: Food[]
+  totalCost: number
+  isPaid: boolean
+  hopperId: string
+  orderId: string
+  methodOfPayment: string
+}
+
+export type Order = {
+  orderList: SubOrder[]
+  additionalCost: number
+  totalFoodCost: number
+  costLimit: number
+  status: string
+  orderId: string
+  ordererId: string
+  comments: string
+  methodOfPayment: string
+}
+
 export enum SUPPER_ACTIONS {
   SET_IS_LOADING = 'SUPPER_ACTIONS.SET_IS_LIADING',
 }
