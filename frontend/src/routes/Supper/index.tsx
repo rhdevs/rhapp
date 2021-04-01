7da1ad3c1956c505bda682e7185867e20fc42030
xinyee20 <codelearner19@gmail.com>
2021-04-02 04:08:49 +0800

    Round Progress Component

--- frontend/src/routes/Supper/index.tsx
+++ frontend/src/routes/Supper/index.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 import { SmileOutlined } from '@ant-design/icons'
 import { StatusSymbol } from '../../components/Supper/StatusSymbol'
+import { RoundProgress } from '../../components/Supper/RoundProgress'
 
 export default function Supper() {
-  return <StatusSymbol leftIcon={<SmileOutlined />} preText="est." text="$10.70 (Equal)" />
+  return (
+    <>
+      <StatusSymbol leftIcon={<SmileOutlined />} preText="est." text="$10.70 (Equal)" />
+      <RoundProgress amountLeft={50} percent={100} />
+    </>
+  )
 }
