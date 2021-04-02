86f04a289b3dcc39f73438e29b7787498a18eb8f
xinyee20 <codelearner19@gmail.com>
2021-04-02 16:17:24 +0800

    Make button color more flexible

--- frontend/src/components/Supper/PlusButton.tsx
+++ frontend/src/components/Supper/PlusButton.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import { PlusCircleFilled } from '@ant-design/icons'
 
-export const PlusButton = () => {
+type Props = {
+  color?: string
+}
+
+export const PlusButton = (props: Props) => {
+  const DARK_BLUE = '#002642'
+  const LIGHT_RED = '#EB5757'
+
+  let BUTTON_COLOR = LIGHT_RED
+
+  if (props.color === 'DARK_BLUE') {
+    BUTTON_COLOR = DARK_BLUE
+  }
+
   return (
     <PlusCircleFilled
       style={{
-        color: '#EB5757',
+        color: BUTTON_COLOR, //'#EB5757'
         fontSize: '30px',
         paddingRight: '7px',
         filter: 'drop-shadow(2px 2.5px 3px rgba(0, 0, 0, 0.15))',
