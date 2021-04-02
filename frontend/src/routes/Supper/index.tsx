6ce21e0cbed66ad2ff6eebd995977ea4741fd281
hui444 <e0425552@u.nus.edu>
2021-04-02 16:47:17 +0800

    Add underlined button

--- frontend/src/routes/Supper/index.tsx
+++ frontend/src/routes/Supper/index.tsx
@@ -4,6 +4,7 @@ import { SmileOutlined } from '@ant-design/icons'
 import { StatusSymbol } from '../../components/Supper/StatusSymbol'
 import { RoundProgress } from '../../components/Supper/RoundProgress'
 import { MainSGCard } from '../../components/Supper/MainSGCard'
+import { UnderlinedButton } from '../../components/Supper/UnderlinedButton'
 
 export default function Supper() {
   return (
@@ -11,6 +12,7 @@ export default function Supper() {
       <RoundProgress amountLeft={50} percent={100} />
       <StatusSymbol isClicked backgroundColor="bluegrey" leftIcon={<SmileOutlined />} preText="est." text="Ordered" />
       <MainSGCard title="f> SUPPER FRIENDS" time="11:59PM" users={17} orderId="RHSO#1002" />
+      <UnderlinedButton text="Add Item" color="red" />
     </>
   )
 }
