303ddf4ca293aa9f3501536468d0fcdaea2c70cc
hui444 <e0425552@u.nus.edu>
2021-04-04 04:29:25 +0800

    Clean code

--- frontend/src/components/Supper/OrderSummaryCard.tsx
+++ frontend/src/components/Supper/OrderSummaryCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import styled from 'styled-components'
-import { Food, Option } from '../../store/supper/types'
+import { Food } from '../../store/supper/types'
 import Button from '../Mobile/Button'
 import { FoodLineInCard } from './FoodLineInCard'
 import { MainCard } from './MainCard'
