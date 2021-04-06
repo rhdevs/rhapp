c0cbfd8a96b274b18e2109164cf4b5a2c79f22d3
xinyee20 <codelearner19@gmail.com>
2021-04-07 04:37:33 +0800

    Add in props

--- frontend/src/components/Supper/NotificationBar.tsx
+++ frontend/src/components/Supper/NotificationBar.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import CloseIcon from '../../assets/CloseIcon.svg'
 
-const MainContainer = styled.div``
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 5px 10px;
+  position: sticky;
+  border-radius: 10px;
+  max-width: 300px;
+  boxshadow: 0px 3px 6px -4px rgba(0, 0, 0, 0.12), 0px 6px 16px rgba(0, 0, 0, 0.08),
+    0px 9px 28px 8px rgba(0, 0, 0, 0.05);
+`
 
 const HeaderContainer = styled.div``
+
+const TextContainer = styled.div`
+  display: flex;
+  flexdirection: column;
+  padding: 10px;
+`
+
+const IconContainer = styled.div`
+  padding: 0px 5px;
+`
 
 const SupperGroupName = styled.text`
   font-size: 14px;
@@ -14,24 +38,32 @@ const TitleContent = styled.text`
   font-size: 14px;
   font-weight: 300;
 `
-
-const TextContainer = styled.text`
+const SubHeader = styled.text`
   font-size: 13px;
   font-weight: 200;
 `
+const CloseButton = styled.img``
 
 type Props = {
   supperGroupName: string
+  onViewClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void
+  onCloseClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void
 }
+
 export const NotificationBar = (props: Props) => {
   return (
     <>
       <MainContainer>
-        <HeaderContainer>
-          <SupperGroupName>{props.supperGroupName}</SupperGroupName>
-          <TitleContent>has been updated!</TitleContent>
-        </HeaderContainer>
-        <TextContainer>Tap to view.</TextContainer>
+        <TextContainer onClick={props.onViewClick}>
+          <HeaderContainer>
+            <SupperGroupName>{props.supperGroupName}</SupperGroupName>
+            <TitleContent> has been updated!</TitleContent>
+          </HeaderContainer>
+          <SubHeader>Tap to view.</SubHeader>
+        </TextContainer>
+        <IconContainer onClick={props.onCloseClick}>
+          <CloseButton src={CloseIcon} alt="close symbol" />
+        </IconContainer>
       </MainContainer>
     </>
   )
