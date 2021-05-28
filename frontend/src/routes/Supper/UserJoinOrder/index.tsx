dc36b0f5ab53c9e7cf9dc5c3843e62f1a337d67b
xinyee20 <codelearner19@gmail.com>
2021-05-29 03:17:46 +0800

    Link to store

--- frontend/src/routes/Supper/UserJoinOrder/index.tsx
+++ frontend/src/routes/Supper/UserJoinOrder/index.tsx
@@ -1,12 +1,14 @@
 import { ShareAltOutlined } from '@ant-design/icons'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import styled from 'styled-components'
 import BottomNavBar from '../../../components/Mobile/BottomNavBar'
 import Button from '../../../components/Mobile/Button'
 import TopNavBar from '../../../components/Mobile/TopNavBar'
 import { JoinOrderSGCard } from '../../../components/Supper/CustomCards/JoinOrderSGCard'
-import { SplitACMethod } from '../../../store/supper/types'
+import { readableSupperGroupId, unixTo12HourTime } from '../../../store/supper/action'
+import { RootState } from '../../../store/types'
 
 const Background = styled.div`
   height: 100vh;
@@ -24,21 +26,22 @@ const ButtonContainer = styled.div`
 
 export default function UserJoinOrder() {
   const rightIcon = <ShareAltOutlined />
+  const { supperGroup } = useSelector((state: RootState) => state.supper)
 
   return (
     <Background>
       <TopNavBar title="Join Order" rightComponent={rightIcon} />
       <JoinOrderSGCard
-        title="f> SUPPER FRIENDS"
-        restaurant="MacDonald"
-        orderId="RHSO#1002"
-        username="Zhou BaoBao"
-        currentAmount={50}
-        priceLimit={100}
-        closingTime="11:59PM"
-        numberOfUsers={7}
-        splitACType={SplitACMethod.EQUAL}
-        deliveryFee="10.70"
+        title={supperGroup?.supperGroupName ?? ''}
+        restaurant={supperGroup?.restaurantName}
+        orderId={readableSupperGroupId(supperGroup?.supperGroupId)}
+        username={supperGroup?.ownerName ?? ''}
+        currentAmount={supperGroup?.currentFoodCost ?? 0}
+        priceLimit={supperGroup?.costLimit ?? 50}
+        closingTime={unixTo12HourTime(supperGroup?.closingTime)}
+        numberOfUsers={supperGroup?.userIdList.length ?? 0}
+        splitACType={supperGroup?.splitAdditionalCost}
+        deliveryFee={'$' + String((supperGroup?.additionalCost ?? 0).toFixed(2))}
       />
       <ButtonContainer>
         <Button defaultButtonDescription="Join Order" stopPropagation={true} />
