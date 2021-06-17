17d33715c2b8f69249b5d238dfed2e3cc604887b
hui444 <e0425552@u.nus.edu>
2021-06-17 20:57:20 +0800

    Update add/edit component to fix no limit case

--- frontend/src/routes/Supper/componentsPage.tsx
+++ frontend/src/routes/Supper/componentsPage.tsx
@@ -83,7 +83,6 @@ export default function Supper() {
       <DeliveryTimeSetter default={20} />
       <QuantityTracker default={5} />
       <SGStatusBubble text={SupperGroupStatus.ORDERED} />
-      ksdfajkhdkjfhskldf
       <RoundProgress currentAmount={10} />
       <MainSGCard title="f> SUPPER FRIENDS" time="11:59PM" users={17} orderId="RHSO#1002" />
       <SGCardWithStatus
