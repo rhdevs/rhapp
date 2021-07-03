5e9b7dd90f50d4c3b6ac0fb20c55a4a5c1a7bb85
xinyee20 <codelearner19@gmail.com>
2021-07-04 03:16:21 +0800

    remove redundant code

--- frontend/src/components/Supper/InformationCard.tsx
+++ frontend/src/components/Supper/InformationCard.tsx
@@ -1,8 +1,8 @@
-import { InfoCircleOutlined } from '@ant-design/icons'
 import React from 'react'
 
 import styled from 'styled-components'
 import { MainCard } from './MainCard'
+import { InfoCircleOutlined } from '@ant-design/icons'
 
 const SymbolContainer = styled.div`
   padding: 2px 10px 0 10px;
@@ -24,16 +24,13 @@ type Props = {
 }
 
 export const InformationCard = (props: Props) => {
-  const shortText = 'This is a short text.'
-  const longText =
-    'Hello this is a notification or an instruction box I think it can be both I am trying to make this message real long to test out and see if it still looks nice so dun mind me, anyeong and move on with life thank you very much.'
   return (
     <MainCard margin="20px ;" borderRadius="10px" padding="20px 10px;">
       <SymbolContainer>
         <InformationSymbol />
       </SymbolContainer>
       <InformationContainer>
-        <InformationText>{shortText}</InformationText>
+        <InformationText>{props.content}</InformationText>
       </InformationContainer>
     </MainCard>
   )
