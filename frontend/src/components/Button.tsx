c4dd848c101b2e4f37fd181c2bbb754705b47d57
tinenhao <tinenhao@u.nus.edu>
2022-01-23 20:05:08 +0800

    added button component

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Border = styled.button<{ state: string }>`
+  height: 2.2rem;
+  width: 10vw;
+  border: transparent;
+  border-radius: 2rem;
+  padding-left: 0.05rem;
+  padding-right: 0.055rem;
+  background: ${(props) =>
+    props.state === 'primary' || props.state === 'secondary'
+      ? `linear-gradient(to left, #72BA75, #58B994)`
+      : '#BFBFBF'};
+`
+const Inner = styled.div<{ state: string }>`
+  height: 100%;
+  width: 100%;
+  border-radius: 2rem;
+  padding-top: 0.35rem;
+  background: ${(props) => (props.state === 'primary' ? `linear-gradient(to left, #72BA75, #58B994)` : 'white')};
+  color: ${(props) => (props.state === 'primary' ? `white` : props.state === 'secondary' ? `#58B994` : '#BFBFBF')};
+`
+type Props = {
+  state: 'disabled' | 'primary' | 'secondary'
+}
+
+function ButtonComponent(prop: Props) {
+  return (
+    <Border state={prop.state}>
+      <Inner state={prop.state}>Test</Inner>
+    </Border>
+  )
+}
+
+export default ButtonComponent
