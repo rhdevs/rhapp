6a1a6e96e9524b45bf8099ef68aa9697272be5b8
Jia Yixuan <jyxhazcake@yahoo.com>
2022-01-24 00:08:44 +0800

    Created gym status component

---
+++ frontend/src/components/GymStatus.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+`
+const Ellipse = styled.div<{ isOpen: boolean }>`
+  background: ${(props) => (props.isOpen ? `rgba(88, 185, 148, 1)` : `rgba(243, 117, 98, 1)`)};
+  height: 16px;
+  width: 16px;
+  border-radius: 50%;
+`
+const Status = styled.div<{ isOpen: boolean }>`
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 26px;
+  letter-spacing: -0.015em;
+  padding-left: 8px;
+
+  background: ${(props) =>
+    props.isOpen ? `linear-gradient(90.67deg, #58b994 30.89%, #72ba75 68.41%)` : `rgba(243, 117, 98, 1)`};
+  background-size: 100%;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+interface Props {
+  isOpen: boolean
+}
+
+function GymStatus(props: Props) {
+  return (
+    <Container>
+      <Ellipse isOpen={props.isOpen} />
+      <Status isOpen={props.isOpen}>{props.isOpen ? 'Gym is Open' : 'Gym is Closed'}</Status>
+    </Container>
+  )
+}
+
+export default GymStatus
