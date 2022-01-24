cf46890b05800187ed92fcebcff23ee63de64068
Jason <jason.ng.zq99@gmail.com>
2022-01-24 17:10:20 +0800

    Add bottom nav bar

--- frontend/src/routes/FacilityBooking/Calendar/index.tsx
+++ frontend/src/routes/FacilityBooking/Calendar/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import BottomNavBar from '../../../components/Mobile/BottomNavBar'
 
 export default function Calendar() {
-  return <>Calendar Page</>
+  return (
+    <>
+      <>Calendar Page</>
+      <BottomNavBar />
+    </>
+  )
 }
