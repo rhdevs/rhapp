bf1a5e441cbad444142bc1527e8bc68212fbad66
Jason <jason.ng.zq99@gmail.com>
2022-01-24 19:20:32 +0800

    Add top nav bar and button component

--- frontend/src/routes/FacilityBooking/Calendar/index.tsx
+++ frontend/src/routes/FacilityBooking/Calendar/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+import ButtonComponent from '../../../components/Button'
 import BottomNavBar from '../../../components/Mobile/BottomNavBar'
+import TopNavBar from '../../../components/Mobile/TopNavBar'
+import { redirect } from '../../../store/route/action'
+import VIEW_MY_BOOKINGS from '../../Routes'
+import { useDispatch } from 'react-redux'
+
+const TopContainer = styled.div`
+  height: 10vh;
+`
 
 export default function Calendar() {
+  const dispatch = useDispatch()
   return (
     <>
-      <>Calendar Page</>
+      <TopContainer>
+        <TopNavBar title={'Calendar'} />
+        <ButtonComponent
+          state={'primary'}
+          text={'Bookings'}
+          onClick={(function (): void {
+            dispatch(redirect(VIEW_MY_BOOKINGS))
+          })}
+        >
+          Booking
+        </ButtonComponent>
+      </TopContainer>
       <BottomNavBar />
     </>
   )
