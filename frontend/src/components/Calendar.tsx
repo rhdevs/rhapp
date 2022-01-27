b4e33d941006ec6bed2cf0de291027c82d996fa3
limwenfeng <E0406421@u.nus.edu>
2022-01-28 01:21:21 +0800

    remove white border and fix styling aligment

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -30,27 +30,29 @@ const DayContainer = styled.div`
 
 const DateContainer = styled.div<{ blurred?: boolean; selected?: boolean }>`
   font-size: 12px;
-  padding-top: 6px;
+  padding-top: auto;
+  padding-bottom: auto;
   text-align: center;
   height: 40px;
   width: 47.14px;
   color: ${(prop) => (prop.blurred ? '#d4d4d4' : prop.selected ? 'white' : '')};
-  border: 5px solid white;
+  border: 0.5px solid white;
   border-radius: 40px;
   background-color: ${(prop) => (prop.selected ? '#468751' : ' white')};
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 `
 
 const EventIndicator = styled.div<{ selected?: boolean }>`
   position: absolute;
-  margin-left: 15px;
-  margin-top: -4px;
+  align-self: center;
+  margin-top: -24px;
   height: 7px;
   width: 7px;
   border-radius: 50%;
   background-color: ${(prop) => (prop.selected ? 'white' : 'black')};
 `
-
-const DateContainerDate = styled.text<{ selected?: boolean }>``
 
 const ClickableDateContainer = (date: number, isBlurred: boolean) => {
   const [dateSelected, isDateSelected] = useState(false)
