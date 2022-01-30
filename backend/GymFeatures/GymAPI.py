faf826f8373c6d0e113d83c58fd1c7d7956362a3
vinatsx <e0775349@u.nus.edu>
2022-01-31 01:33:05 +0800

    pull up past 3 days records

--- backend/GymFeatures/GymAPI.py
+++ backend/GymFeatures/GymAPI.py
@@ -3,17 +3,25 @@ from flask import Blueprint, make_response
 import pymongo
 import sys
 from datetime import datetime
+import time
 sys.path.append("../")
 
 gym_api = Blueprint("gym", __name__)
 
 @gym_api.route("/")
 def get_all_history():
+    unix3days = 259200
     try:
-        data = list(db.Gym.find({},{"_id":0}))
+        pipeline1 = [{"$addFields":{'date_diff': {"$subtract" : [int(time.time()), '$requesttime']}}},
+            {"$project":{"_id":0,'userID': 1, 'telegramHandle':1, 'requesttime':1, 'keyStatus':1, 'gymStatus':1, 'lte3days': {"$toString":{"$lte" : ["$date_diff",unix3days]}}}},
+            {"$match":{'lte3days':"true"}},
+            {"$project":{'lte3days':0}}]
+        
+        data = list(db.Gym.aggregate(pipeline1))
         response = {"status":"success","data":data}
 
     except Exception as e:
-        return {"err":"An error has occured","status":"failed"},500
+        print(e)
+        return {"err":"An error has occured", "status":"failed"}, 500
 
     return make_response(response)
