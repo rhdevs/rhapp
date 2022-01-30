994de3a24945e2d166dc78deff07e0dd85321237
vinatsx <e0775349@u.nus.edu>
2022-01-30 23:26:22 +0800

    retrieve all gym records in db

--- backend/GymFeatures/GymAPI.py
+++ backend/GymFeatures/GymAPI.py
@@ -1,5 +1,5 @@
 from db import *
-from flask import Blueprint
+from flask import Blueprint, make_response
 import pymongo
 import sys
 from datetime import datetime
@@ -8,5 +8,12 @@ sys.path.append("../")
 gym_api = Blueprint("gym", __name__)
 
 @gym_api.route("/")
-def hello():
-    return "Welcome"
+def get_all_history():
+    try:
+        data = list(db.Gym.find({},{"_id":0}))
+        response = {"status":"success","data":data}
+
+    except Exception as e:
+        return {"err":"An error has occured","status":"failed"},500
+
+    return make_response(response)
