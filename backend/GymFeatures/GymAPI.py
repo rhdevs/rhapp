1174c145fa4847248dd36e22036ab69f779a9d8c
vinatsx <e0775349@u.nus.edu>
2022-02-02 16:58:44 +0800

    check for user record within 60s

--- backend/GymFeatures/GymAPI.py
+++ backend/GymFeatures/GymAPI.py
@@ -4,6 +4,7 @@ import pymongo
 import sys
 from datetime import datetime
 import time
+import pandas as pd
 sys.path.append("../")
 
 gym_api = Blueprint("gym", __name__)
@@ -16,12 +17,18 @@ def get_all_history():
             {"$project":{"_id":0,'userID': 1, 'telegramHandle':1, 'requesttime':1, 'keyStatus':1, 'gymStatus':1, 'lte3days': {"$toString":{"$lte" : ["$date_diff",unix3days]}}}},
             {"$match":{'lte3days':"true"}},
             {"$project":{'lte3days':0}}]
-        
+
         data = list(db.Gym.aggregate(pipeline1))
+        data = pd.DataFrame(data)
+        checktelegramHandle = (data["telegramHandle"] == data["telegramHandle"].shift(-1))
+        checkrequesttime = data['requesttime'].shift(-1) - data['requesttime'] <= 60
+        data = data[~((checktelegramHandle) & (checkrequesttime))]
+        data = data.to_dict('records')
         response = {"status":"success","data":data}
 
     except Exception as e:
         print(e)
         return {"err":"An error has occured", "status":"failed"}, 500
 
+    
     return make_response(response)
