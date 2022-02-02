c368a2e83ab1529a11eba4df09e460af47faac7f
botakveggie <minhanchoo13@gmail.com>
2022-02-03 01:36:26 +0800

    added togglegym and keywithuser

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ import os
 import pymongo
 from Social.SocialAPI import social_api
 from FacilityBooking.FacilitiesAPI import facilities_api
+from GymFeatures.GymAPI import gym_api
 from Scheduling.SchedulingAPI import scheduling_api
 from Supper.SupperAPI import supper_api
 from Authentication.AuthenticationAPI import authentication_api
@@ -21,6 +22,7 @@ app.register_blueprint(facilities_api, url_prefix="/facilities")
 app.register_blueprint(scheduling_api, url_prefix="/scheduling")
 app.register_blueprint(supper_api, url_prefix="/supper")
 app.register_blueprint(authentication_api, url_prefix="/auth")
+app.register_blueprint(gym_api, url_prefix="/gym")
 
 
 @app.route("/")
