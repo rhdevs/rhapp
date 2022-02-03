1f9b5aba7b956b29c946e899c7a84ebd53980c6c
botakveggie <minhanchoo13@gmail.com>
2022-02-03 11:16:29 +0800

    reordered functions for tidiness

--- backend/GymFeatures/GymAPI.py
+++ backend/GymFeatures/GymAPI.py
@@ -12,6 +12,35 @@ sys.path.append("../")
 
 gym_api = Blueprint("gym", __name__)
 DEFAULT_KEY_LOC = "5-409"
+@gym_api.route("/", methods = ['GET'])
+def get_all_history():
+    unix3days = 259200
+    try:
+        pipeline1 = [{"$addFields":{'date_diff': {"$subtract" : [int(time.time()), '$requesttime']}}},
+            {"$project":{"_id":0,'userID': 1, 'telegramHandle':1, 'requesttime':1, 'keyStatus':1, 'gymStatus':1, 'lte3days': {"$toString":{"$lte" : ["$date_diff",unix3days]}}}},
+            {"$match":{'lte3days':"true"}},
+            {"$project":{'lte3days':0}}]
+
+        data = list(db.Gym.aggregate(pipeline1))
+        data = pd.DataFrame(data)
+        checktelegramHandle = (data["telegramHandle"] == data["telegramHandle"].shift(-1))
+        checkrequesttime = data['requesttime'].shift(-1) - data['requesttime'] <= 60
+        data = data[~((checktelegramHandle) & (checkrequesttime))]
+        data = data.to_dict('records')
+        response = {"status":"success","data":data}
+    except Exception as e:
+        print(e)
+        return {"err":"An error has occured", "status":"failed"}, 500
+    return make_response(response)
+
+@gym_api.route("/status", methods=['GET'])
+def get_statuses():
+    try:
+        data = list(db.Gym.find({}, {"_id": 0, "gymStatus": 1, "keyStatus": 1}).sort("requesttime", -1))
+        response = {"data": data[0], "status": "success"}
+    except:
+        return {"err": "An error has occurred", "status": "failed"}, 500
+    return make_response(response)
 
 @gym_api.route("/movekey", methods = ["POST"])
 def move_key():
@@ -48,55 +77,6 @@ def toggle_gym():
     except Exception as e:
         print(e)
         return {"err":"An error has occured", "status":"failed"}, 500
-
-# @gym_api.route("/now")
-# def gym_now():
-#     try:
-#         data = db.Gym.find().sort('_id',-1).limit(1).next()
-#         key_loc = data["keyStatus"]
-#         is_open = data["gymStatus"]
-#         if is_open:
-#             gym_status = "open"
-#         else:
-#             gym_status = "closed"
-#         response = {"data":{"Key":key_loc, "is_open":is_open},"status":"success","message":f"key at {key_loc}, gym is {gym_status}"}
-#         return make_response(response)
-#     except Exception as e:
-#         print(e)
-#         return {"err":"An error has occured", "status":"failed"}, 500
-
-@gym_api.route("/", methods = ['GET'])
-def get_all_history():
-    unix3days = 259200
-    try:
-        pipeline1 = [{"$addFields":{'date_diff': {"$subtract" : [int(time.time()), '$requesttime']}}},
-            {"$project":{"_id":0,'userID': 1, 'telegramHandle':1, 'requesttime':1, 'keyStatus':1, 'gymStatus':1, 'lte3days': {"$toString":{"$lte" : ["$date_diff",unix3days]}}}},
-            {"$match":{'lte3days':"true"}},
-            {"$project":{'lte3days':0}}]
-
-        data = list(db.Gym.aggregate(pipeline1))
-        data = pd.DataFrame(data)
-        checktelegramHandle = (data["telegramHandle"] == data["telegramHandle"].shift(-1))
-        checkrequesttime = data['requesttime'].shift(-1) - data['requesttime'] <= 60
-        data = data[~((checktelegramHandle) & (checkrequesttime))]
-        data = data.to_dict('records')
-        response = {"status":"success","data":data}
-
-    except Exception as e:
-        print(e)
-        return {"err":"An error has occured", "status":"failed"}, 500
-
-    
-    return make_response(response)
-
-@gym_api.route("/status", methods=['GET'])
-def get_statuses():
-    try:
-        data = list(db.Gym.find({}, {"_id": 0, "gymStatus": 1, "keyStatus": 1}).sort("requesttime", -1))
-        response = {"data": data[0], "status": "success"}
-    except:
-        return {"err": "An error has occurred", "status": "failed"}, 500
-    return make_response(response)
 
 @gym_api.route("/gymstatus", methods=['POST'])
 def add_gymStatus():
