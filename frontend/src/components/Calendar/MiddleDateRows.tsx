f0d52ec34ce95c474f31b2be656ee20f7ef94793
limwenfeng <E0406421@u.nus.edu>
2022-02-06 01:07:37 +0800

    replacing duplicates code with for loop and mapper

--- frontend/src/components/Calendar/MiddleDateRows.tsx
+++ frontend/src/components/Calendar/MiddleDateRows.tsx
@@ -2,29 +2,19 @@ import React from 'react'
 import { ClickableDateContainer } from './ClickableDateContainer'
 
 export const MiddleDateRows = (firstDate: number, assignedMonth: number, eventDates: number[]) => {
+  const days: number[] = []
+
+  for (let i = firstDate; i < 28; i++) {
+    days.push(i)
+  }
+
   return (
     <>
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
-      <ClickableDateContainer date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
+      {days.map((day) => {
+        return (
+          <ClickableDateContainer key={day} date={firstDate++} assignedMonth={assignedMonth} eventDates={eventDates} />
+        )
+      })}
     </>
   )
 }
