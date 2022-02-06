a9b72b9837b70de7e97b25ef22b027e9e3b49e40
Jason <jason.ng.zq99@gmail.com>
2022-02-06 23:48:36 +0800

    Improve code quality to ClickableDateContainer.tsx

--- frontend/src/components/Calendar/ClickableDateContainer.tsx
+++ frontend/src/components/Calendar/ClickableDateContainer.tsx
@@ -8,23 +8,23 @@ const DateContainer = styled.div<{ selected?: boolean; currentDate?: boolean }>`
   text-align: center;
   height: 40px;
   width: 47.14px;
-  color: ${(prop) => (prop.selected ? 'white' : prop.currentDate ? '#58B994' : '')};
+  color: ${(props) => (props.selected ? 'white' : props.currentDate ? '#58B994' : '')};
   border-radius: 40px;
-  background-color: ${(prop) => (prop.selected ? '#468751' : !prop.selected && prop.currentDate ? '#D8E6DF' : '')};
+  background-color: ${(props) => (props.selected ? '#468751' : props.currentDate ? '#D8E6DF' : '')};
   display: flex;
   justify-content: center;
   flex-direction: column;
 `
 
 const EventIndicator = styled.div<{ selected?: boolean; eventPresent?: boolean }>`
-  display: ${(prop) => (prop.eventPresent ? 'block' : 'none')};
+  display: ${(props) => (props.eventPresent ? 'block' : 'none')};
   align-self: center;
   margin-top: -6px;
   margin-bottom: -1px;
   height: 7px;
   width: 7px;
   border-radius: 50%;
-  background-color: ${(prop) => (prop.selected ? 'white' : '#468751')};
+  background-color: ${(props) => (props.selected ? 'white' : '#468751')};
 `
 
 export const ClickableDateContainer = (props: {
@@ -40,25 +40,21 @@ export const ClickableDateContainer = (props: {
     console.log('Date ' + assignedDateMonth + ' selected. Need to change color.')
   }
 
-  const checkEventPresence = () => {
-    if (props.eventDates.find((date) => date === assignedDateMonth)) {
-      return true
-    }
+  const hasEvent = () => {
+    return props.eventDates.find((date) => date === assignedDateMonth) !== undefined
   }
 
-  const checkCurrentDate = () => {
+  const isCurrentDate = () => {
     const today = new Date()
     const month = new Date(today).getMonth() + 1
     const day = new Date(today).getDate()
     const processedDate = month * 100 + day
-    if (processedDate === assignedDateMonth) {
-      return true
-    }
+    return processedDate === assignedDateMonth
   }
 
   return (
-    <DateContainer onClick={DateContainerClickHandler} selected={dateSelected} currentDate={checkCurrentDate()}>
-      <EventIndicator selected={dateSelected} eventPresent={checkEventPresence()} />
+    <DateContainer onClick={DateContainerClickHandler} selected={dateSelected} currentDate={isCurrentDate()}>
+      <EventIndicator selected={dateSelected} eventPresent={hasEvent()} />
       {props.date}
     </DateContainer>
   )
