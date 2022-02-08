5783195315511fbe77a220f83e216bd9b652b4a1
Jason <jason.ng.zq99@gmail.com>
2022-02-08 22:15:25 +0800

    Edit variable naming

--- frontend/src/store/calendar/actions.ts
+++ frontend/src/store/calendar/actions.ts
@@ -1,26 +1,23 @@
 import { Dispatch, GetState } from '../types'
 import { ActionTypes, CALENDAR_ACTIONS } from './types'
 
-export const SetIsClicked = (currentlyClickedDate: number) => async (
-  dispatch: Dispatch<ActionTypes>,
-  getState: GetState,
-) => {
+export const SetIsClicked = (newClickedDate: number) => async (dispatch: Dispatch<ActionTypes>, getState: GetState) => {
   const { isClicked, clickedDate } = getState().calendar
   if (isClicked) {
-    if (currentlyClickedDate !== clickedDate) {
+    if (newClickedDate !== clickedDate) {
       return
     } else {
-      console.log('Disabling click now.')
       dispatch({
         type: CALENDAR_ACTIONS.SET_IS_CLICKED,
         newIsClicked: false,
         newClickedDate: 0,
       })
+      console.log('Disabling click now.')
     }
   }
   dispatch({
     type: CALENDAR_ACTIONS.SET_IS_CLICKED,
     newIsClicked: true,
-    newClickedDate: currentlyClickedDate,
+    newClickedDate: newClickedDate,
   })
 }
