fc0fa2ed31d2de3cdc0279b879553bbde153086b
tinenhao <tinenhao@u.nus.edu>
2022-02-13 15:56:35 +0800

    fixed implementation of button, added disabled state and added type argument

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,44 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Border = styled.button<{ state: string }>`
+const Button = styled.button<{ state: string }>`
   height: 2.2rem;
-  width: 10vw;
-  border: transparent;
-  border-radius: 2rem;
-  padding-left: 0rem;
-  padding-right: 0rem;
-  background: linear-gradient(to left, #72ba75, #58b994);
-
-  &:active {
-    background: ${(props) => (props.state === 'primary' ? '#D8E6DF' : '#8B8B8B')};
-    transform: scale(0.95);
-  }
-`
-const Inner = styled.button<{ state: string }>`
-  height: 98%;
-  width: 98%;
-  border-radius: 2rem;
-  border: transparent;
-  background: ${(props) => (props.state === 'primary' ? `linear-gradient(to left, #72BA75, #58B994)` : 'white')};
+  width: 10rem;
+  border-radius: 100rem;
+  font-size: 1rem;
+  border: solid 0.1rem transparent;
+  background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)),
+    linear-gradient(to left, #72ba75, #58b994);
+  box-shadow: ${(props) => (props.state === 'primary' ? `0px` : `2px 1000px 1px #fff inset`)};
   color: ${(props) => (props.state === 'primary' ? `white` : `#58B994`)};
 
   &:active {
-    background: #d8e6df;
+    background: ${(props) => (props.state === 'primary' ? '#D8E6DF' : '#8B8B8B')};
     color: #8b8b8b;
+    transform: scale(0.95);
+  }
+
+  &:disabled {
+    color: #bfbfbf;
+    border-color: #bfbfbf;
+    background: white;
   }
 `
+
 type Props = {
   state: 'primary' | 'secondary'
   text: string
+  disabled?: boolean
+  type?: 'button' | 'submit' | 'reset' | undefined
   onClick: () => void
 }
 
 function ButtonComponent(prop: Props) {
   return (
-    <Border onClick={() => prop.onClick()} state={prop.state}>
-      <Inner state={prop.state}>{prop.text}</Inner>
-    </Border>
+    <Button onClick={() => prop.onClick()} state={prop.state} disabled={prop.disabled} type={prop.type ?? 'button'}>
+      {prop.text}
+    </Button>
   )
 }
 
