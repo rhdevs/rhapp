d5172a7c410b25515fb36f44b4b1a71ccbb92603
vinatsx <e0775349@u.nus.edu>
2022-02-20 15:53:36 +0800

    cleaned up code

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-#from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 import os
 import pymongo
 from Social.SocialAPI import social_api
@@ -10,7 +10,7 @@ from Supper.SupperAPI import supper_api
 from Authentication.AuthenticationAPI import authentication_api
 from GymFeatures.GymAPI import gym_api
 from db import *
-#from gevent.pywsgi import WSGIServer
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 #cors = CORS(app)
@@ -31,6 +31,6 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=8080)
-    #http_server = WSGIServer(('', 8080), app)
-    #http_server.serve_forever()
+    #app.run("0.0.0.0", port=8080)
+    http_server = WSGIServer(('', 8080), app)
+    http_server.serve_forever()
