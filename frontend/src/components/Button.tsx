fff8bff6f1afc2727cee695f978cae41e89881de
Xin Yee <63994179+xinyee20@users.noreply.github.com>
2022-02-24 03:11:37 +0800

    Update Button.tsx

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { colorTheme } from '../theme'
 
 const Border = styled.button<{ state: string }>`
   height: 2.2rem;
@@ -8,7 +9,11 @@ const Border = styled.button<{ state: string }>`
   border-radius: 2rem;
   padding-left: 0rem;
   padding-right: 0rem;
-  background: linear-gradient(to left, #72ba75, #58b994);
+  background: linear-gradient(
+    to left,
+    ${colorTheme.button.positive.background.gradientLeft},
+    ${colorTheme.button.positive.background.gradientRight}
+  );
 
   &:active {
     background: ${(props) => (props.state === 'primary' ? '#D8E6DF' : '#8B8B8B')};
@@ -20,8 +25,12 @@ const Inner = styled.button<{ state: string }>`
   width: 98%;
   border-radius: 2rem;
   border: transparent;
-  background: ${(props) => (props.state === 'primary' ? `linear-gradient(to left, #72BA75, #58B994)` : 'white')};
-  color: ${(props) => (props.state === 'primary' ? `white` : `#58B994`)};
+  background: ${(props) =>
+    props.state === 'primary'
+      ? `linear-gradient(to left, ${colorTheme.button.positive.background.gradientLeft},
+    ${colorTheme.button.positive.background.gradientRight})`
+      : 'white'};
+  color: ${(props) => (props.state === 'primary' ? colorTheme.button.positive.font : colorTheme.button.negative.font)};
 
   &:active {
     background: #d8e6df;
