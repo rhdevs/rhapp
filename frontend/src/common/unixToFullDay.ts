56709c4c173433d80f7d2eb7d81078bc25bbb194
limwenfeng <E0406421@u.nus.edu>
2022-03-27 00:25:16 +0800

    update function documentation

--- frontend/src/common/unixToFullDay.ts
+++ frontend/src/common/unixToFullDay.ts
@@ -3,7 +3,7 @@ import { days } from './dates'
 /**
  *
  * @param unixDate epoch/unix date time number
- * @returns time in the form of (dd month yyyy) (eg, 27 April 2022)
+ * @returns time in the form of Monday, Tuesday etc
  */
 export const unixToFullDay = (unixDate: number) => {
   const date = new Date(unixDate * 1000)
