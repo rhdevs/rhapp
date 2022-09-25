a84a17626fbd6d3a3f723984fde7ff3d269700bd
joshualeejunyi <joshua.lee.99@hotmail.com>
2022-09-25 22:32:52 +0800

    added docs for hourblocks component

--- frontend/src/components/FacilityBooking/HourBlocks.tsx
+++ frontend/src/components/FacilityBooking/HourBlocks.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 import { MainTimeContainer, TimeContainer } from './BlockStyles.styled'
 
+/**
+ * @returns A column of each hour from 0000 to 2400, to be displayed next to DailyContainer containing time blocks
+ * @example
+ * ```
+ * <HourBlocks/>
+ * ```
+ */
 const HourBlocks = () => {
   return (
     <MainTimeContainer>
