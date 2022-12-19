176565bc554723414b2df0df455bd1e6bec87174
woowenjun99 <woowenjun99@gmail.com>
2022-12-19 22:39:32 +0800

    TBC Social action

--- frontend/src/store/social/action.ts
+++ frontend/src/store/social/action.ts
@@ -1,36 +1,46 @@
 import axios from 'axios'
 import { Dispatch, GetState } from '../types'
 import { ActionTypes, SOCIAL_ACTIONS, POSTS_FILTER } from './types'
-import { DOMAIN_URL, ENDPOINTS, DOMAINS, post, put, get, del } from '../endpoints'
+import { DOMAIN_URL, ENDPOINTS, DOMAINS, post, get, del } from '../endpoints'
 import { cloneDeep, difference, sortBy } from 'lodash'
 import useSnackbar from '../../hooks/useSnackbar'
 import { fetchUserPosts } from '../profile/action'
+import type { Position, Post } from './types'
 
 const [success] = useSnackbar('success')
 const [error] = useSnackbar('error')
 
-export const getUserDetail = () => (dispatch: Dispatch<ActionTypes>) => {
-  const userID = localStorage.getItem('userID')
-  get(ENDPOINTS.USER, DOMAINS.SOCIAL, '/' + userID).then((response) => {
-    if (response.data === '' || response.data === undefined) {
-      console.log(response.err)
-    } else {
-      const data = response.data[0]
-      dispatch({
-        type: SOCIAL_ACTIONS.GET_USER_DETAIL,
-        userId: data.userID,
-        avatar: data.profilePictureUrl,
-        name: data.displayName,
-        position: data.position,
-        telegramHandle: data.telegramHandle,
-      })
-    }
-  })
-}
+export const getUserDetail =
+  () =>
+  async (dispatch: Dispatch<ActionTypes>): Promise<void> => {
+    // Get the token and send a GET request to the server
+    const userID = localStorage.getItem('userID')
+    const { data } = await axios.get<
+      {
+        displayName: string
+        userID: string
+        position: Position[]
+        profilePictureUrl: string | undefined
+        telegramHandle: string
+      }[]
+    >(DOMAINS.SOCIAL + ENDPOINTS.USER + '/' + userID)
 
-export const GetPostDetailsToEdit = () => (dispatch: Dispatch<ActionTypes>, getState: GetState) => {
-  const { postId } = getState().social
-  dispatch(GetSpecificPost(postId)).then(() => {
+    // Dispatch the data
+    dispatch({
+      type: SOCIAL_ACTIONS.GET_USER_DETAIL,
+      userId: data[0].userID,
+      avatar: data[0].profilePictureUrl,
+      name: data[0].displayName,
+      position: data[0].position,
+      telegramHandle: data[0].telegramHandle,
+    })
+  }
+
+export const GetPostDetailsToEdit =
+  () =>
+  async (dispatch: Dispatch<ActionTypes>, getState: GetState): Promise<void> => {
+    const { postId } = getState().social
+    await dispatch(GetSpecificPost(postId))
     const { viewPost } = getState().social
     const { title, description, postPics, isOfficial, userId } = viewPost
     dispatch({
@@ -43,225 +53,226 @@ export const GetPostDetailsToEdit = () => (dispatch: Dispatch<ActionTypes>, getS
       newPostCca: 0,
       userId: userId,
     })
-  })
-}
-
-export const ResetPostDetails = () => (dispatch: Dispatch<ActionTypes>, getState: GetState) => {
-  const { position } = getState().social
-
-  dispatch({
-    type: SOCIAL_ACTIONS.EDIT_NEW_FIELDS,
-    newPostTitle: '',
-    newPostBody: '',
-    newPostImages: [],
-    newPostOfficial: false,
-    newPostCca: 0,
-  })
-}
-
-export const handleEditPost = () => (dispatch: Dispatch<ActionTypes>, getState: GetState) => {
-  const { newPostTitle, newPostBody, newPostOfficial, viewPost, newPostImages, newPostCca } = getState().social
-  const requestBody = {
-    postID: viewPost.postId,
-    ccaID: newPostCca,
-    title: newPostTitle,
-    description: newPostBody,
-    isOfficial: newPostOfficial,
-    postPics: newPostImages,
-    tags: [],
   }
-  put(ENDPOINTS.EDIT_POST, DOMAINS.SOCIAL, requestBody).then(() => {
+
+export const ResetPostDetails =
+  () =>
+  (dispatch: Dispatch<ActionTypes>): void => {
+    dispatch({
+      type: SOCIAL_ACTIONS.EDIT_NEW_FIELDS,
+      newPostTitle: '',
+      newPostBody: '',
+      newPostImages: [],
+      newPostOfficial: false,
+      newPostCca: 0,
+    })
+  }
+
+export const handleEditPost =
+  () =>
+  async (dispatch: Dispatch<ActionTypes>, getState: GetState): Promise<void> => {
+    const { newPostTitle, newPostBody, newPostOfficial, viewPost, newPostImages, newPostCca } = getState().social
+    const requestBody = {
+      postID: viewPost.postId,
+      ccaID: newPostCca,
+      title: newPostTitle,
+      description: newPostBody,
+      isOfficial: newPostOfficial,
+      postPics: newPostImages,
+      tags: [],
+    }
+
+    await axios.put(DOMAINS.SOCIAL + ENDPOINTS.EDIT_POST, requestBody)
     dispatch(GetPosts(POSTS_FILTER.ALL))
     success('Post edited!')
-  })
-}
-
-export const handleCreatePost = () => (dispatch: Dispatch<ActionTypes>, getState: GetState) => {
-  const { newPostTitle, newPostBody, newPostOfficial, newPostImages, newPostCca } = getState().social
-
-  const requestBody = {
-    title: newPostTitle,
-    description: newPostBody,
-    userID: localStorage.getItem('userID'),
-    isOfficial: newPostOfficial,
-    postPics: newPostImages ?? [],
-    ccaID: newPostCca,
-    tags: [],
   }
-  post(ENDPOINTS.CREATE_POSTS, DOMAINS.SOCIAL, requestBody).then(() => {
+
+export const handleCreatePost =
+  () =>
+  async (dispatch: Dispatch<ActionTypes>, getState: GetState): Promise<void> => {
+    const { newPostTitle, newPostBody, newPostOfficial, newPostImages, newPostCca } = getState().social
+
+    const requestBody = {
+      title: newPostTitle,
+      description: newPostBody,
+      userID: localStorage.getItem('userID'),
+      isOfficial: newPostOfficial,
+      postPics: newPostImages ?? [],
+      ccaID: newPostCca,
+      tags: [],
+    }
+
+    await axios.post(DOMAINS.SOCIAL + ENDPOINTS.CREATE_POSTS, requestBody)
     dispatch(GetPosts(POSTS_FILTER.ALL))
     success('Post created!')
-  })
-}
-
-export const DeleteImage = (urlToDelete: string) => (dispatch: Dispatch<ActionTypes>, getState: GetState) => {
-  const { newPostTitle, newPostBody, newPostOfficial, newPostCca } = getState().social
-  let { newPostImages } = getState().social
-  newPostImages = newPostImages?.filter((url) => {
-    return url !== urlToDelete
-  })
-
-  dispatch({
-    type: SOCIAL_ACTIONS.EDIT_NEW_FIELDS,
-    newPostTitle: newPostTitle,
-    newPostBody: newPostBody,
-    newPostImages: newPostImages,
-    newPostOfficial: newPostOfficial,
-    newPostCca: newPostCca,
-  })
-}
-
-export const AddImage = (e: React.ChangeEvent<HTMLInputElement>) => (
-  dispatch: Dispatch<ActionTypes>,
-  getState: GetState,
-) => {
-  dispatch({ type: SOCIAL_ACTIONS.SET_IS_UPLOADING, isUploading: true })
-  const { warnings, newPostImages } = getState().social
-  e.preventDefault()
-
-  const reader = new FileReader()
-  const files = e.target.files ?? []
-  const file = files.length > 0 ? files[0] : null
-  if (!file) {
-    warnings.push('No file selected!')
-  } else if (!file.type.match('image.*')) {
-    warnings.push('File is not a photo!')
-  } else {
-    reader.readAsDataURL(file)
-    reader.onloadend = () => {
-      if (newPostImages.length > 2) {
-        warnings.push('The limit is 3 photos!')
-      }
-      // TODO: Upload images to cloud, and delete
-      const newUrl =
-        reader.result && !(reader.result instanceof ArrayBuffer) && newPostImages.length < 3
-          ? [...newPostImages, reader.result]
-          : newPostImages
-
-      dispatch({
-        type: SOCIAL_ACTIONS.ADD_IMAGE,
-        newPostImages: newUrl,
-      })
-    }
-  }
-  dispatch({ type: SOCIAL_ACTIONS.SET_IS_UPLOADING, isUploading: false })
-  dispatch({
-    type: SOCIAL_ACTIONS.SET_WARNINGS,
-    warnings: warnings,
-  })
-}
-
-export const PopWarning = () => (dispatch: Dispatch<ActionTypes>, getState: GetState) => {
-  const { warnings } = getState().social
-  warnings.pop()
-  dispatch({
-    type: SOCIAL_ACTIONS.SET_WARNINGS,
-    warnings: warnings,
-  })
-}
-
-export const EditPostDetail = (fieldName: string, fieldData: string) => (
-  dispatch: Dispatch<ActionTypes>,
-  getState: GetState,
-) => {
-  let { newPostTitle, newPostBody, newPostOfficial, newPostCca } = getState().social
-  const { newPostImages } = getState().social
-
-  switch (fieldName) {
-    case 'title':
-      newPostTitle = fieldData
-      break
-    case 'body':
-      newPostBody = fieldData
-      break
-    case 'official':
-      newPostOfficial = !newPostOfficial
-      break
-    case 'postImage':
-      if (fieldData) {
-        newPostImages.push(fieldData)
-      }
-      break
-    case 'cca':
-      if (fieldData) {
-        newPostCca = parseInt(fieldData)
-      }
-      break
   }
 
-  dispatch({
-    type: SOCIAL_ACTIONS.EDIT_NEW_FIELDS,
-    newPostTitle: newPostTitle,
-    newPostBody: newPostBody,
-    newPostImages: newPostImages,
-    newPostOfficial: newPostOfficial,
-    newPostCca: newPostCca,
-  })
-}
+export const DeleteImage =
+  (urlToDelete: string) =>
+  (dispatch: Dispatch<ActionTypes>, getState: GetState): void => {
+    const { newPostTitle, newPostBody, newPostOfficial, newPostCca } = getState().social
+    let { newPostImages } = getState().social
+    newPostImages = newPostImages?.filter((url) => {
+      return url !== urlToDelete
+    })
 
-export const GetPosts = (postFilter: POSTS_FILTER, limit?: number, userId?: string) => async (
-  dispatch: Dispatch<ActionTypes>,
-  getState: GetState,
-) => {
-  dispatch(SetIsLoading(true))
-  let endpoint: ENDPOINTS
-  const { posts } = getState().social
-
-  switch (postFilter) {
-    case POSTS_FILTER.OFFICIAL:
-      endpoint = ENDPOINTS.OFFICIAL_POSTS
-      break
-    case POSTS_FILTER.ALL:
-      endpoint = ENDPOINTS.ALL_POSTS
-      break
-    case POSTS_FILTER.FRIENDS:
-      endpoint = ENDPOINTS.FRIENDS_OF_USER_POSTS
-      break
-    default:
-      endpoint = ENDPOINTS.ALL_POSTS
-      break
+    dispatch({
+      type: SOCIAL_ACTIONS.EDIT_NEW_FIELDS,
+      newPostTitle: newPostTitle,
+      newPostBody: newPostBody,
+      newPostImages: newPostImages,
+      newPostOfficial: newPostOfficial,
+      newPostCca: newPostCca,
+    })
   }
 
-  // const subroute: string = postFilter != POSTS_FILTER.OFFICIAL ? `?N=${limit}&userID=${userId}` : `?N=${limit}`
-  const subroute = userId && limit ? `?N=${limit}&userID=${userId}` : limit ? `?N=${limit}` : ``
+export const AddImage =
+  (e: React.ChangeEvent<HTMLInputElement>) =>
+  (dispatch: Dispatch<ActionTypes>, getState: GetState): void => {
+    dispatch({ type: SOCIAL_ACTIONS.SET_IS_UPLOADING, isUploading: true })
+    const { warnings, newPostImages } = getState().social
+    e.preventDefault()
 
-  get(endpoint, DOMAINS.SOCIAL, subroute).then((response) => {
-    if (response.data.length > 0) {
-      const transformedPost = cloneDeep(response.data).map((post) => {
-        post.date = post.createdAt
-        post.postId = post.postID
-        post.ccaId = post.ccaID
-        post.userId = post.userID
-        post.date = new Date(post.createdAt)
-        post.profilePic = post.profilePictureURI
-        return post
-      })
-
-      //validate if caller made repeated call to the same posts
-      const transformedPostID = transformedPost.map((post) => post.postId)
-      const postIds = posts.map((post) => post.postId)
-      const postDiff = difference(transformedPostID, postIds)
-
-      if (postDiff.length > 0) {
-        const diffTransformedPosts = transformedPost.filter((post) => postDiff.includes(post.postId))
-        const sortedPosts = sortBy(diffTransformedPosts.concat(posts), ['postId']).reverse()
-        dispatch({
-          type: SOCIAL_ACTIONS.GET_POSTS,
-          posts: sortedPosts,
-        })
-      } else {
-        //do nothing
-        //repeated call so do not concat same posts to existing posts
-      }
+    const reader = new FileReader()
+    const files = e.target.files ?? []
+    const file = files.length > 0 ? files[0] : null
+    if (!file) {
+      warnings.push('No file selected!')
+    } else if (!file.type.match('image.*')) {
+      warnings.push('File is not a photo!')
     } else {
-      dispatch({
-        type: SOCIAL_ACTIONS.SET_HAS_NO_MORE_POSTS,
-        hasNoMorePosts: true,
-      })
+      reader.readAsDataURL(file)
+      reader.onloadend = () => {
+        if (newPostImages.length > 2) {
+          warnings.push('The limit is 3 photos!')
+        }
+        // TODO: Upload images to cloud, and delete
+        const newUrl =
+          reader.result && !(reader.result instanceof ArrayBuffer) && newPostImages.length < 3
+            ? [...newPostImages, reader.result]
+            : newPostImages
+
+        dispatch({
+          type: SOCIAL_ACTIONS.ADD_IMAGE,
+          newPostImages: newUrl,
+        })
+      }
     }
-    dispatch(SetIsLoading(false))
-  })
-}
+    dispatch({ type: SOCIAL_ACTIONS.SET_IS_UPLOADING, isUploading: false })
+    dispatch({
+      type: SOCIAL_ACTIONS.SET_WARNINGS,
+      warnings: warnings,
+    })
+  }
+
+export const PopWarning =
+  () =>
+  (dispatch: Dispatch<ActionTypes>, getState: GetState): void => {
+    const { warnings } = getState().social
+    warnings.pop()
+    dispatch({
+      type: SOCIAL_ACTIONS.SET_WARNINGS,
+      warnings: warnings,
+    })
+  }
+
+export const EditPostDetail =
+  (fieldName: string, fieldData: string) =>
+  (dispatch: Dispatch<ActionTypes>, getState: GetState): void => {
+    let { newPostTitle, newPostBody, newPostOfficial, newPostCca } = getState().social
+    const { newPostImages } = getState().social
+
+    switch (fieldName) {
+      case 'title':
+        newPostTitle = fieldData
+        break
+      case 'body':
+        newPostBody = fieldData
+        break
+      case 'official':
+        newPostOfficial = !newPostOfficial
+        break
+      case 'postImage':
+        if (fieldData) {
+          newPostImages.push(fieldData)
+        }
+        break
+      case 'cca':
+        if (fieldData) {
+          newPostCca = parseInt(fieldData)
+        }
+        break
+    }
+
+    dispatch({
+      type: SOCIAL_ACTIONS.EDIT_NEW_FIELDS,
+      newPostTitle: newPostTitle,
+      newPostBody: newPostBody,
+      newPostImages: newPostImages,
+      newPostOfficial: newPostOfficial,
+      newPostCca: newPostCca,
+    })
+  }
+
+export const GetPosts =
+  (postFilter: POSTS_FILTER, limit?: number, userId?: string) =>
+  async (dispatch: Dispatch<ActionTypes>, getState: GetState): Promise<void> => {
+    dispatch(SetIsLoading(true))
+    let endpoint: ENDPOINTS
+    const { posts } = getState().social
+
+    switch (postFilter) {
+      case POSTS_FILTER.OFFICIAL:
+        endpoint = ENDPOINTS.OFFICIAL_POSTS
+        break
+      case POSTS_FILTER.ALL:
+        endpoint = ENDPOINTS.ALL_POSTS
+        break
+      case POSTS_FILTER.FRIENDS:
+        endpoint = ENDPOINTS.FRIENDS_OF_USER_POSTS
+        break
+      default:
+        endpoint = ENDPOINTS.ALL_POSTS
+        break
+    }
+
+    // const subroute: string = postFilter != POSTS_FILTER.OFFICIAL ? `?N=${limit}&userID=${userId}` : `?N=${limit}`
+    const subroute = userId && limit ? `?N=${limit}&userID=${userId}` : limit ? `?N=${limit}` : ``
+
+    get(endpoint, DOMAINS.SOCIAL, subroute).then((response) => {
+      if (response.data.length > 0) {
+        const transformedPost = cloneDeep(response.data).map((post) => {
+          post.date = post.createdAt
+          post.postId = post.postID
+          post.ccaId = post.ccaID
+          post.userId = post.userID
+          post.date = new Date(post.createdAt)
+          post.profilePic = post.profilePictureURI
+          return post
+        })
+
+        //validate if caller made repeated call to the same posts
+        const transformedPostID = transformedPost.map((post) => post.postId)
+        const postIds = posts.map((post) => post.postId)
+        const postDiff = difference(transformedPostID, postIds)
+
+        if (postDiff.length > 0) {
+          const diffTransformedPosts: Post[] = transformedPost.filter((post) => postDiff.includes(post.postId))
+          const sortedPosts = sortBy(diffTransformedPosts.concat(posts), ['postId']).reverse()
+          dispatch({
+            type: SOCIAL_ACTIONS.GET_POSTS,
+            posts: sortedPosts,
+          })
+        }
+      } else {
+        dispatch({
+          type: SOCIAL_ACTIONS.SET_HAS_NO_MORE_POSTS,
+          hasNoMorePosts: true,
+        })
+      }
+      dispatch(SetIsLoading(false))
+    })
+  }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const DeletePost = (postIdToDelete: string) => async (dispatch: Dispatch<any>, getState: GetState) => {
@@ -301,52 +312,74 @@ export const SetPostId = (postId: string) => (dispatch: Dispatch<ActionTypes>) =
   })
 }
 
-export const GetSpecificPost = (postId: string) => async (dispatch: Dispatch<ActionTypes>) => {
-  const response = await axios.get(`${DOMAIN_URL.SOCIAL}${ENDPOINTS.SPECIFIC_POST}?postID=${postId}`)
-  const specificPost = response.data.data
+export const GetSpecificPost =
+  (postId: string) =>
+  async (dispatch: Dispatch<ActionTypes>): Promise<void> => {
+    const response = await axios.get<{
+      data: {
+        ccaID: number
+        createdAt: string
+        description: string
+        isOfficial: boolean
+        name: string
+        postID: string
+        postPics: string[]
+        profilePic: string
+        title: string
+        userID: string
+      }
+    }>(`${DOMAIN_URL.SOCIAL}${ENDPOINTS.SPECIFIC_POST}?postID=${postId}`)
+    const specificPost = response.data.data
 
-  const { postID, title, createdAt, ccaID, isOfficial, description, postPics, name, userID, profilePic } = specificPost
-  const newPost = {
-    name: name,
-    userId: userID,
-    postId: postID,
-    title: title,
-    isOfficial: isOfficial,
-    ccaId: ccaID,
-    description: description,
-    postPics: postPics,
-    createdAt: createdAt,
-    profilePic: profilePic,
+    const { postID, title, createdAt, ccaID, isOfficial, description, postPics, name, userID, profilePic } =
+      specificPost
+    const newPost = {
+      name: name,
+      userId: userID,
+      postId: postID,
+      title: title,
+      isOfficial: isOfficial,
+      ccaId: ccaID,
+      description: description,
+      postPics: postPics,
+      createdAt: createdAt,
+      profilePic: profilePic,
+    }
+    dispatch({
+      type: SOCIAL_ACTIONS.GET_SPECIFIC_POST,
+      viewPost: newPost,
+    })
   }
-  dispatch({
-    type: SOCIAL_ACTIONS.GET_SPECIFIC_POST,
-    viewPost: newPost,
-  })
-}
 
-export const IncreasePageIndex = () => (dispatch: Dispatch<ActionTypes>, getState: GetState) => {
-  const { pageIndex, postsFilter } = getState().social
-  const { userID } = getState().profile.user
+export const IncreasePageIndex =
+  () =>
+  (dispatch: Dispatch<ActionTypes>, getState: GetState): void => {
+    const { pageIndex, postsFilter } = getState().social
+    const { userID } = getState().profile.user
 
-  const newPageIndex = pageIndex + 1
+    const newPageIndex = pageIndex + 1
 
-  dispatch(GetPosts(postsFilter, newPageIndex, userID))
-  dispatch({
-    type: SOCIAL_ACTIONS.INCREASE_PAGE_INDEX,
-    pageIndex: newPageIndex,
-  })
-}
+    dispatch(GetPosts(postsFilter, newPageIndex, userID))
+    dispatch({
+      type: SOCIAL_ACTIONS.INCREASE_PAGE_INDEX,
+      pageIndex: newPageIndex,
+    })
+  }
 
-export const SetIsLoading = (isLoading: boolean) => (dispatch: Dispatch<ActionTypes>) => {
-  dispatch({
-    type: SOCIAL_ACTIONS.SET_IS_LOADING,
-    isLoading: isLoading,
-  })
-}
+export const SetIsLoading =
+  (isLoading: boolean) =>
+  (dispatch: Dispatch<ActionTypes>): void => {
+    dispatch({
+      type: SOCIAL_ACTIONS.SET_IS_LOADING,
+      isLoading: isLoading,
+    })
+  }
 
-export const SetHasNoMorePosts = (hasNoMorePosts: boolean) => (dispatch: Dispatch<ActionTypes>) => {
-  dispatch({
-    type: SOCIAL_ACTIONS.SET_HAS_NO_MORE_POSTS,
-    hasNoMorePosts: hasNoMorePosts,
-  })
-}
+export const SetHasNoMorePosts =
+  (hasNoMorePosts: boolean) =>
+  (dispatch: Dispatch<ActionTypes>): void => {
+    dispatch({
+      type: SOCIAL_ACTIONS.SET_HAS_NO_MORE_POSTS,
+      hasNoMorePosts: hasNoMorePosts,
+    })
+  }
