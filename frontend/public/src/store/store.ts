72c7cb6af1b38438c49cd66c5182e27470fe28a0
woowenjun99 <woowenjun99@gmail.com>
2022-12-19 18:26:59 +0800

    Remove unused packages

--- frontend/public/src/store/store.ts
+++ frontend/public/src/store/store.ts
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-// import _ from 'lodash'
 
 export const rootReducer = combineReducers({})
 
