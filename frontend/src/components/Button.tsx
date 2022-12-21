cb9e9775844e4ad3219f0f96a0403d7c13a4366c
Marcus Ong <marcus.ong.qy@gmail.com>
2022-12-21 13:52:48 +0800

    minor code cleanup

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -11,15 +11,15 @@ const StyledButton = styled.button<{
 }>`
   min-height: 35px;
   height: ${(props) => props.height ?? 'auto'};
-  width: ${(props) => props.width ?? `max-content`};
+  width: ${(props) => props.width ?? 'max-content'};
   border-radius: 25px;
   font-size: ${(props) => props.fontSize ?? '12px'};
   border: solid 0.1rem transparent;
   background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)),
     linear-gradient(to left, #72ba75, #58b994);
-  box-shadow: ${(props) => (props.state === 'primary' ? `0px` : `2px 1000px 1px #fff inset`)};
-  color: ${(props) => (props.state === 'primary' ? `white` : `#58B994`)};
-  padding: ${(props) => (props.size === 'small' ? `7px 14px` : `16px 30px`)};
+  box-shadow: ${(props) => (props.state === 'primary' ? '0px' : '2px 1000px 1px #fff inset')};
+  color: ${(props) => (props.state === 'primary' ? 'white' : '#58B994')};
+  padding: ${(props) => (props.size === 'small' ? '7px 14px' : '16px 30px')};
   line-height: ${(props) => props.lineHeight ?? '1px'};
   white-space: normal;
   cursor: pointer;
@@ -40,9 +40,9 @@ const StyledButton = styled.button<{
 type Props = {
   state: 'primary' | 'secondary'
   text: string
+  onClick: React.MouseEventHandler<HTMLButtonElement>
   disabled?: boolean
   type?: 'button' | 'submit' | 'reset' | undefined
-  onClick: React.MouseEventHandler<HTMLButtonElement>
   width?: string | undefined
   height?: string | undefined
   fontSize?: string | undefined
@@ -54,13 +54,13 @@ type Props = {
  *
  * @param state ('primary' | 'secondary') Primary button is filled green, secondary is white (hollow)
  * @param text (string) Text to be displayed on the button
- * @param disabled (boolean) If the button is disabled, it will be greyed out, and not clickable
- * @param type ('button' | 'submit' | 'reset' | undefined) Type of button, default is 'button'
  * @param onClick (React.MouseEventHandler<HTMLButtonElement>) Function to be called when button is clicked
- * @param width (string | undefined) Width of the button, default is max-content
- * @param height (string | undefined) Height of the button, default is max-content
- * @param fontSize (string | undefined) Font size of the text of the button, default is max-content
- * @param size ('small' | 'regular' | undefined) Size of the button, default is regular
+ * @param disabled (optional) (boolean) If the button is disabled, it will be greyed out, and not clickable
+ * @param type (optional) ('button' | 'submit' | 'reset' | undefined) Type of button, default is 'button'
+ * @param width (optional) (string | undefined) Width of the button, default is max-content
+ * @param height (optional) (string | undefined) Height of the button, default is max-content
+ * @param fontSize (optional) (string | undefined) Font size of the text of the button, default is max-content
+ * @param size (optional) ('small' | 'regular' | undefined) Size of the button, default is regular
  * @returns a button
  */
 
--- frontend/src/routes/FacilityBooking/FacilityLandingPage/index.tsx
+++ frontend/src/routes/FacilityBooking/FacilityLandingPage/index.tsx
@@ -1,28 +1,31 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
+
 import { PATHS } from '../../Routes'
 import ButtonComponent from '../../../components/Button'
 import BottomNavBar from '../../../components/Mobile/BottomNavBar'
 import TopNavBar from '../../../components/Mobile/TopNavBar'
 
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 154px;
+  width: 297px;
+`
+const LandingPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+  height: 234px;
+  margin-top: 63px;
+`
+
 export default function FacilityLandingPage() {
   const history = useHistory()
-  const SearchContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    height: 154px;
-    width: 297px;
-  `
-  const LandingPageContainer = styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    flex-direction: column;
-    width: 100%;
-    height: 234px;
-    margin: 63px 0px 0px 0px;
-  `
+
   return (
     <>
       <TopNavBar title="Faclility Booking" />
